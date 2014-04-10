e47fc72bed5abbf3633afd9f145aa2f98d0af352
Aram Price <aprice@pivotallabs.com>
2014-04-10 16:07:32 -0700

    Remove all local .pivotal files before building a new one
    - this is a workaround for Jenkins archiving which is blunt

--- scripts/build_pivotal.sh
+++ scripts/build_pivotal.sh
@@ -3,8 +3,11 @@
 set -e
 
 P_RUNTIME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+P_RUNTIME_METADATA=${P_RUNTIME_DIR}/metadata/cf.yml
+
+rm -f ${P_RUNTIME_DIR}/*.pivotal
 
 bundle install
 bundle exec vara-build-metadata --product-dir="${P_RUNTIME_DIR}"
-bundle exec vara-download-artifacts --product-metadata="${P_RUNTIME_DIR}/metadata/cf.yml"
-bundle exec vara-build-pivotal --product-metadata="${P_RUNTIME_DIR}/metadata/cf.yml" --rc="-build${BUILD_NUMBER:--local}"
+bundle exec vara-download-artifacts --product-metadata="${P_RUNTIME_METADATA}"
+bundle exec vara-build-pivotal      --product-metadata="${P_RUNTIME_METADATA}" --rc="-build${BUILD_NUMBER:--local}"
