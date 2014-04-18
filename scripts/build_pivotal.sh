72f23def74d3132b18fd25a5a398c69b7dab9734
Aram Price <aprice@pivotallabs.com>
2014-04-18 12:24:33 -0700

    Normalize build script, update .gitignore
    Signed-off-by: Mark Rushakoff <mrushakoff@pivotallabs.com>

--- scripts/build_pivotal.sh
+++ scripts/build_pivotal.sh
@@ -2,17 +2,17 @@
 
 set -e
 
-P_RUNTIME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-P_RUNTIME_METADATA=${P_RUNTIME_DIR}/metadata/cf.yml
+PRODUCT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+METADATA_FILE=${PRODUCT_DIR}/metadata/cf.yml
 
 echo "------------- metadata_parts/binaries.yml -------------"
-cat ${P_RUNTIME_DIR}/metadata_parts/binaries.yml
+cat ${PRODUCT_DIR}/metadata_parts/binaries.yml
 echo "------------- metadata_parts/binaries.yml -------------"
 
-rm -f ${P_RUNTIME_DIR}/*.pivotal
-rm -f ${P_RUNTIME_DIR}/*.pivotal.yml
+rm -f ${PRODUCT_DIR}/*.pivotal
+rm -f ${PRODUCT_DIR}/*.pivotal.yml
 
 bundle install
-bundle exec vara-build-metadata --product-dir="${P_RUNTIME_DIR}"
-bundle exec vara-download-artifacts --product-metadata="${P_RUNTIME_METADATA}"
-bundle exec vara-build-pivotal      --product-metadata="${P_RUNTIME_METADATA}" --rc="-build${BUILD_NUMBER:--local}"
+bundle exec vara-build-metadata     --product-dir="${PRODUCT_DIR}"
+bundle exec vara-download-artifacts --product-metadata="${METADATA_FILE}"
+bundle exec vara-build-pivotal      --product-metadata="${METADATA_FILE}" --rc="-build${BUILD_NUMBER:--local}"
