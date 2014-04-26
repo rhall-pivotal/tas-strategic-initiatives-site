a7ac4d8fdb1dd1ce83d36b63c7820d206128dd4e
Brian Cunnie <cunnie@pivotallabs.com>
2014-04-25 17:21:16 -0700

    Clean up .pivotal.md5 files in build script
    Signed-off-by: Mark Rushakoff <mrushakoff@pivotallabs.com>

--- scripts/build_pivotal.sh
+++ scripts/build_pivotal.sh
@@ -11,6 +11,7 @@ echo "------------- metadata_parts/binaries.yml -------------"
 
 rm -f ${PRODUCT_DIR}/*.pivotal
 rm -f ${PRODUCT_DIR}/*.pivotal.yml
+rm -f ${PRODUCT_DIR}/*.pivotal.md5
 
 bundle install
 bundle exec vara-build-metadata     --product-dir="${PRODUCT_DIR}"
