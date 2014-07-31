e41378232805c0f392eec307bd201ab629b780d6
Jenny Chou <jchou@pivotallabs.com>
2014-07-31 16:05:49 -0700

    Remove the lvl_2_compiled directory
    We aren't following this pattern anymore.
    
    Signed-off-by: Mark Rushakoff <mrushakoff@pivotallabs.com>

--- lvl_2_compiled/scripts/build_pivotal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../../scripts && pwd )"
-
-PRODUCT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-
-source ${SCRIPTS_DIR}/shared.sh
