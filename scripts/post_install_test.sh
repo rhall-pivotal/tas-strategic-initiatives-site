cdf8f25f5b9cf500b2baa31dad6c51f1338d58c9
Aakash Shah <ashah@pivotallabs.com>
2014-10-01 11:22:07 -0700

    Correctly place CATs on the GOPATH
    Signed-off-by: Mark Rushakoff <mrushakoff@pivotallabs.com>

--- scripts/post_install_test.sh
+++ scripts/post_install_test.sh
@@ -1,43 +1,23 @@
 #!/bin/bash
 # Warning: Running CATs (this test) may pollute your deployment
 
-set -x
+set -ex
 
 export SCRIPT_DIR=$(dirname $0)
 export SKIP_SSL_VALIDATION=true
 
-function set_cats_sha() {
-  export CF_RELEASE_TAG=$(ruby -r 'yaml' -e 'puts "v" + YAML.load_file(ENV["SCRIPT_DIR"] + "/../metadata_parts/binaries.yml").fetch("releases").find{ |release| release["name"] == "cf"}.fetch("version")')
-  export CATS_DIR='src/acceptance-tests'
+PCF_RELEASE_REF=$(ruby -r 'yaml' -e 'puts "v" + YAML.load_file(ENV["SCRIPT_DIR"] + "/../metadata_parts/binaries.yml").fetch("releases").find{ |release| release["name"] == "cf"}.fetch("version")')
 
-  cd ${GOPATH}
-  mkdir -p src/github.com/pivotal-cf/
-  cd src/github.com/pivotal-cf/
-  if [ -d pcf-release ]; then
-    cd pcf-release
-    git fetch origin master
-  else
-    git clone git@github.com:pivotal-cf/pcf-release.git
-    cd pcf-release
-  fi
+rm -rf pcf-release
+git clone -b ${PCF_RELEASE_REF} git@github.com:pivotal-cf/pcf-release.git
+cd pcf-release
 
-  export CATS_SHA=$(git ls-tree ${CF_RELEASE_TAG} ${CATS_DIR} | awk '{print $3}')
-}
+DEST_DIR="${GOPATH}/src/github.com/cloudfoundry/cf-acceptance-tests/"
 
-set_cats_sha
-
-cd $GOPATH
-mkdir -p src/github.com/cloudfoundry/
-cd src/github.com/cloudfoundry/
-if [ -d cf-acceptance-tests ]; then
-  cd cf-acceptance-tests
-  git pull origin master
-else
-  git clone git@github.com:cloudfoundry/cf-acceptance-tests.git
-  cd cf-acceptance-tests
-fi
-
-git checkout $CATS_SHA
+git submodule update --init --recursive -- 'src/acceptance-tests'
+rm -rf ${DEST_DIR}
+mv src/acceptance-tests/ ${DEST_DIR}
+cd ${DEST_DIR}
 
 cat > integration_config.json <<EOF
 {
