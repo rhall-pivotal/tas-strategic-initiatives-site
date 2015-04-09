90dfd0cc99c75791bdb5cdb0e7ee3beb59334f73
Joseph Palermo <jpalermo@pivotal.io>
2015-04-09 14:50:31 -0700

    Publish docker images to bosh riak and s3 registries

--- docker_images/build_and_upload_image.sh
+++ docker_images/build_and_upload_image.sh
@@ -3,10 +3,17 @@ set -e
 
 DOCKER_IMAGE_NAME=releng
 IMAGE_FOLDER="$( cd "$( dirname ${BASH_SOURCE[0]} )"/${DOCKER_IMAGE_NAME} && pwd )"
-DOCKER_REGISTRY=docker.vsphere.gocd.cf-app.com:5000
+DOCKER_REGISTRY_VSPHERE=docker.vsphere.gocd.cf-app.com:5000
+DOCKER_REGISTRY_AWS=docker.gocd.cf-app.com:5000
 
-docker build -t ${DOCKER_REGISTRY}/releng/${DOCKER_IMAGE_NAME} ${IMAGE_FOLDER}
-echo "Build finished for ${DOCKER_IMAGE_NAME}"
+docker build -t ${DOCKER_REGISTRY_VSPHERE}/releng/${DOCKER_IMAGE_NAME} ${IMAGE_FOLDER}
+echo "Build finished for ${DOCKER_IMAGE_NAME} in vsphere"
 
-docker push ${DOCKER_REGISTRY}/releng/${DOCKER_IMAGE_NAME}
-echo "Finished uploading image ${DOCKER_IMAGE_NAME} to ${DOCKER_REGISTRY}"
+docker push ${DOCKER_REGISTRY_VSPHERE}/releng/${DOCKER_IMAGE_NAME}
+echo "Finished uploading image ${DOCKER_IMAGE_NAME} to ${DOCKER_REGISTRY_VSPHERE}"
+
+docker build -t ${DOCKER_REGISTRY_AWS}/releng/${DOCKER_IMAGE_NAME} ${IMAGE_FOLDER}
+echo "Build finished for ${DOCKER_IMAGE_NAME} in aws"
+
+docker push ${DOCKER_REGISTRY_AWS}/releng/${DOCKER_IMAGE_NAME}
+echo "Finished uploading image ${DOCKER_IMAGE_NAME} to ${DOCKER_REGISTRY_AWS}"
