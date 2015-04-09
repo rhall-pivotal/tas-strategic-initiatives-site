5307754fff89a64c39bbbfb6990a6e99d5dcb92d
Joseph Palermo <jpalermo@pivotal.io>
2015-04-09 11:19:22 -0700

    Generic run script for docker

---
+++ scripts/run_in_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+WORKSPACE_DIR="$( cd ${SCRIPT_DIR}/../.. && pwd )"
+
+SSH_KEY_LOCATION=${SSH_KEY_LOCATION:-/var/vcap/jobs/gocd-agent/id_rsa}
+
+DOCKER_REGISTRY=${DOCKER_REGISTRY:-docker.vsphere.gocd.cf-app.com:5000}
+DOCKER_IMAGE=${DOCKER_IMAGE:-releng/releng}
+
+docker run \
+  -v ${SSH_KEY_LOCATION}:/root/.ssh/id_rsa \
+  -v ${WORKSPACE_DIR}:/workspace \
+  -w "/workspace/p-runtime" \
+  ${DOCKER_REGISTRY}/${DOCKER_IMAGE} $@
