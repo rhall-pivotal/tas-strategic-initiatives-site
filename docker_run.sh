96b7b1447165edc85ad25e4be59d1dd0baf81c71
Dave Liebreich <dliebreich@pivotal.io>
2015-05-13 16:26:33 -0700

    Add files necessary to build a Docker image in a pipeline
    - Also add ability to use Docker image in subsequent pipelines
    - Docker images are now tagged with a git revision
    - Images are pushed to AWS and vSphere docker registries
    
    [#94200598]
    
    Signed-off-by: Morgan Fine <mfine@pivotal.io>

---
+++ docker_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+workspace_dir="$( cd ${script_dir}/.. && pwd )"
+
+docker run --privileged \
+  -v ${workspace_dir}:/workspace \
+  -e AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY \
+  -e RUNTIME_DOT_PIVOTAL_FILE \
+  -e ENVIRONMENTS_DIR=config/environments \
+  -w "/workspace/p-runtime" \
+  -t -i ${DOCKER_REGISTRY}/releng:`git describe --dirty` /bin/sh -c "$*"
