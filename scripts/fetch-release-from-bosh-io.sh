fc8737a3952da7ae9bda15b9ce000314cb7fb35f
Morgan Fine <mfine@pivotal.io>
2015-06-02 10:19:41 -0700

    fetch-release-from-bosh-io.sh doesn't fail when release not found
    - We use this script to prepopulate our cf release in the releases
    directory of p-runtime as it is faster than vara. However, for patch
    releases, which aren't on bosh.io, we can't use the script. This commit
    allows us to keep the script in place in both scenarios.

--- scripts/fetch-release-from-bosh-io.sh
+++ scripts/fetch-release-from-bosh-io.sh
@@ -1,5 +1,3 @@
-set -e
-
 SCRIPT_DIR=$( cd "$( dirname $0 )" && pwd)
 RELEASE_NAME=$1
 BOSH_IO_RELEASE_NAME=$2
@@ -16,3 +14,15 @@ fi
 
 set -x
 ${SCRIPT_DIR}/run_in_docker.sh aria2c -x 5 --out=releases/${RELEASE_FILE} http://bosh.io/d/github.com/cloudfoundry/${BOSH_IO_RELEASE_NAME}?v=${RELEASE_VERSION}
+
+ret_val=$?
+
+# aria2c return value 3 -> resource not found
+
+if [ $ret_val -eq 3 ]
+then
+  echo "${RELEASE_FILE} not found on bosh.io - falling back to vara fetch"
+  exit 0
+fi
+
+exit $ret_val
