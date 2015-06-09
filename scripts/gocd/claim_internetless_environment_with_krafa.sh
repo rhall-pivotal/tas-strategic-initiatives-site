cb8c832a8d35d32dcc7d8f24ce9504e6dcdc8279
Dave Liebreich <dliebreich@pivotal.io>
2015-06-09 11:57:56 -0700

    use krafa_client instead of rake tasks [#92970082]

--- scripts/gocd/claim_internetless_environment_with_krafa.sh
+++ scripts/gocd/claim_internetless_environment_with_krafa.sh
@@ -1,3 +1,3 @@
 set -e
-./docker_run.sh bundle exec rake --trace krafa:claim_without_internet[${GO_PIPELINE_NAME}-${GO_PIPELINE_COUNTER}]
-echo "export RELENG_ENV=$(./docker_run.sh bundle exec rake --trace krafa:print_environment_name[${GO_PIPELINE_NAME}-${GO_PIPELINE_COUNTER}])" > releng_env.sh
+claimed_environment=$(./docker_run.sh bundle exec krafa_client claim --iaas=${IAAS:-vsphere} --no-has-internet --claim-reason="${GO_PIPELINE_NAME}-${GO_PIPELINE_COUNTER}")
+echo "export RELENG_ENV=$claimed_environment" > releng_env.sh
--- scripts/gocd/release_claim.sh
+++ scripts/gocd/release_claim.sh
@@ -1,1 +1,2 @@
-./docker_run.sh bundle exec rake --trace krafa:release_claim[${GO_PIPELINE_NAME}-${GO_PIPELINE_COUNTER}]
+source ./releng_env.sh
+./docker_run.sh bundle exec krafa_client release_claim --environment-name=$RELENG_ENV
