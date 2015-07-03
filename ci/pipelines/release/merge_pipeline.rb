7902c1c765054df975b62ecf257204d5692869c5
Difan Zhao <dzhao@pivotal.io>
2015-07-02 17:34:18 -0700

    Add ERT-1.6 concourse pipeline yml
    - The ert-1.6-full-pipeline.yml is the result of running
    merge_pipeline.rb which looks at all other .yml files in the release
    pipelines/directory to create a full yml file that defines our master
    ERT-1.6 pipelines
    
    [#97484564]
    
    Signed-off-by: Morgan Fine <mfine@pivotal.io>

---
+++ ci/pipelines/release/merge_pipeline.rb
@@ -0,0 +1,16 @@
+require 'yaml'
+require 'active_support/core_ext/hash/deep_merge'
+
+files=['aws-clean.yml','vsphere-clean.yml', 'vsphere-internetless.yml', 'aws-upgrade.yml', 'vsphere-upgrade.yml']
+
+result = YAML.load_file('ert-1.6.yml')
+files.each do |f|
+  result['jobs'] += YAML.load_file(f)['jobs']
+end
+
+output = YAML.dump(result)
+File.open('ert-1.6-full-pipeline.yml', 'w') do |f|
+  f.write(output)
+end
+
+puts 'ERT-1.6 pipeline config written to ert-1.6-full-pipeline.yml'
