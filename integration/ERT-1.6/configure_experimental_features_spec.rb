a1e82514a89101b36dc250029a4d1398ce10d82e
Morgan Fine <mfine@pivotal.io>
2015-08-14 11:42:41 -0700

    configure_experimental_features disables errands
    - This rake task is now responsible for turning on all experimental
    features and enabling diego as the default backend.
    - Until errands work with diego, the rake task is also responsible for
    disabling the errands that do not work with it.
    
    [#101229604]

--- integration/ERT-1.6/configure_experimental_features_spec.rb
+++ integration/ERT-1.6/configure_experimental_features_spec.rb
@@ -32,8 +32,19 @@ RSpec.describe 'Configure Elastic Runtime 1.6.X Experimental Features', order: :
     diego_form.open_form
 
     diego_form.property('.cloud_controller.default_to_diego_backend').set(true)
-    diego_form.property('.cloud_controller.diego_docker').set(true)
 
     diego_form.save_form
+  end
+
+  it 'disables the errands that do not work with diego' do
+    %w(
+      push-app-usage-service
+      notifications
+      autoscaling
+      autoscaling-register-broker
+    ).each do |errand|
+      current_ops_manager.product(elastic_runtime_settings.name)
+        .product_errands.disable_errand(errand)
+    end
   end
 end
