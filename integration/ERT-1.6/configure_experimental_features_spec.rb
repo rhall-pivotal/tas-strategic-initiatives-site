0bf1626dafb769676dd6cb9998b441fb3ff78966
Morgan Fine <mfine@pivotal.io>
2015-08-21 14:31:46 -0700

    Push-app-usage-service errand should run on apps-manager salmon branch
    Signed-off-by: Difan Zhao <dzhao@pivotal.io>

--- integration/ERT-1.6/configure_experimental_features_spec.rb
+++ integration/ERT-1.6/configure_experimental_features_spec.rb
@@ -24,4 +24,27 @@ RSpec.describe 'Configure Elastic Runtime 1.6.X Experimental Features', order: :
 
     experimental_features_form.save_form
   end
+
+  it 'enables the diego features' do
+    diego_form =
+      current_ops_manager.product(elastic_runtime_settings.name).product_form('diego')
+
+    diego_form.open_form
+
+    diego_form.property('.cloud_controller.default_to_diego_backend').set(true)
+
+    diego_form.save_form
+  end
+
+  it 'disables the errands that do not work with diego' do
+    %w(
+      notifications
+      notifications-ui
+      autoscaling
+      autoscaling-register-broker
+    ).each do |errand|
+      current_ops_manager.product(elastic_runtime_settings.name)
+        .product_errands.disable_errand(errand)
+    end
+  end
 end
