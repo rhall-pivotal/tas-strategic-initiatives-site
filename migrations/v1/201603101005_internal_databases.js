9f5ad7f76959d22e73f1257e5de94724a98b3613
Jesse Alford <jalford@pivotal.io>
2016-03-10 15:19:36 -0800

    Fix database property migration js syntax
    [#112847745]

--- migrations/v1/201603101005_internal_databases.js
+++ migrations/v1/201603101005_internal_databases.js
@@ -1,9 +1,9 @@
 exports.migrate = function(input) {
-  var properties = input['.properties'];
+  var properties = input.properties;
 
   // Migrate mixed internal databases to MySQL-only
-  if (properties['.properties.system_database'].value == "internal") {
-    properties['.properties.system_database'].value = "internal_mysql";
+  if (properties['.properties.system_database']['value'] == "internal") {
+    properties['.properties.system_database']['value'] = "internal_mysql";
   }
 
   return input;
