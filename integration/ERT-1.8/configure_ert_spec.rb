7905765fa00e15f0eb76f4cb457059d48579a23f
Aditya Anchuri <aanchuri@pivotal.io>
2016-06-23 15:02:13 -0700

    Added ability to configure range of TCP routing ports
    - Also refactored ert configure task
    
    [#120641011]
    
    Signed-off-by: Ryan Moran <rmoran@pivotal.io>

--- integration/ERT-1.8/configure_ert_spec.rb
+++ integration/ERT-1.8/configure_ert_spec.rb
@@ -35,16 +35,135 @@ RSpec.describe 'Configure Elastic Runtime 1.8.X', order: :defined do
   end
 
   it 'configures networking' do
+    networking_form = current_ops_manager.product(elastic_runtime_settings['name']).product_form('networking')
+    networking_form.open_form
+
     case env_settings['iaas_type']
     when 'aws'
-      configure_aws_load_balancers(elastic_runtime_settings)
+      system_logging_form = current_ops_manager.product(elastic_runtime_settings['name']).product_form('syslog_aggregator')
+      system_logging_form.open_form
+      system_logging_form.save_form
+
+      networking_form.open_form
+      networking_form.fill_in_selector_property(
+        selector_input_reference: '.properties.tcp_routing',
+        selector_name: 'enable',
+        selector_value: 'enable',
+        sub_field_answers: {
+          '.properties.tcp_routing.enable.reservable_ports' => {
+            attribute_value: '1024-1123',
+          },
+        },
+      )
+      resource_config = current_ops_manager.product_resources_configuration(elastic_runtime_settings['name'])
+      resource_config.set_instances_for_job('ha_proxy', 0)
+      resource_config.set_elb_names_for_job('router', elastic_runtime_settings['elb_name'])
+      resource_config.set_elb_names_for_job('diego_brain', elastic_runtime_settings['ssh_elb_name'])
+
+      # takes advantage of the pcf.<name>.cf-app.com format
+      env_name = env_settings['name'].split('.')[1]
+      tcp_elb_name = env_name + '-pcf-tcp-elb'
+      resource_config.set_elb_names_for_job('tcp-router', tcp_elb_name)
+
+      networking_form.fill_in_selector_property(
+        selector_input_reference: '.properties.networking_point_of_entry',
+        selector_name: '',
+        selector_value: 'external_ssl',
+        sub_field_answers: {}
+      )
+      networking_form.property('.ha_proxy.skip_cert_verify').set(elastic_runtime_settings['trust_self_signed_certificates'])
+      ssl_rsa_cert_property = '.properties.networking_point_of_entry][external_ssl][.properties.networking_point_of_entry.external_ssl.ssl_rsa_certificate'
+      if elastic_runtime_settings['ssl_certificate']
+        networking_form.nested_property(ssl_rsa_cert_property, 'cert_pem').set(elastic_runtime_settings['ssl_certificate'])
+        networking_form.nested_property(ssl_rsa_cert_property, 'private_key_pem').set(elastic_runtime_settings['ssl_private_key'])
+      else
+        domain = elastic_runtime_settings['system_domain']
+        networking_form.generate_self_signed_cert(
+          "*.#{domain},*.login.#{domain},*.uaa.#{domain}",
+          '.properties.networking_point_of_entry.external_ssl.ssl_rsa_certificate',
+          '.properties.networking_point_of_entry',
+          'external_ssl'
+        )
+      end
+
+      networking_form.property('.properties.logger_endpoint_port').set('4443')
     when 'vsphere', 'vcloud'
-      configure_vsphere_ha_proxy(elastic_runtime_settings)
-      configure_vsphere_tcp_routing(elastic_runtime_settings)
+      networking_form.property('.ha_proxy.static_ips').set(elastic_runtime_settings['ha_proxy_static_ips'])
+      networking_form.fill_in_selector_property(
+        selector_input_reference: '.properties.networking_point_of_entry',
+        selector_name: '',
+        selector_value: 'haproxy',
+        sub_field_answers: {}
+      )
+
+      networking_form.property('.ha_proxy.skip_cert_verify').set(elastic_runtime_settings['trust_self_signed_certificates'])
+      ssl_rsa_cert_property = '.properties.networking_point_of_entry][haproxy][.properties.networking_point_of_entry.haproxy.ssl_rsa_certificate'
+      if elastic_runtime_settings['ssl_certificate']
+        networking_form.nested_property(ssl_rsa_cert_property, 'cert_pem').set(elastic_runtime_settings['ssl_certificate'])
+        networking_form.nested_property(ssl_rsa_cert_property, 'private_key_pem').set(elastic_runtime_settings['ssl_private_key'])
+      else
+        domain = elastic_runtime_settings['system_domain']
+        networking_form.generate_self_signed_cert(
+          "*.#{domain},*.login.#{domain},*.uaa.#{domain}",
+          '.properties.networking_point_of_entry.haproxy.ssl_rsa_certificate',
+          '.properties.networking_point_of_entry',
+          'haproxy'
+        )
+      end
+
+      networking_form.fill_in_selector_property(
+        selector_input_reference: '.properties.tcp_routing',
+        selector_name: 'enable',
+        selector_value: 'enable',
+        sub_field_answers: {
+          '.properties.tcp_routing.enable.reservable_ports' => {
+            attribute_value: '1024-1123',
+          },
+        },
+      )
+      networking_form.property('.tcp-router.static_ips').set(elastic_runtime_settings['tcp_router_static_ips'])
     when 'openstack'
-      configure_openstack_ha_proxy(elastic_runtime_settings)
-      configure_openstack_tcp_router(elastic_runtime_settings)
+      networking_form.fill_in_selector_property(
+        selector_input_reference: '.properties.tcp_routing',
+        selector_name: 'enable',
+        selector_value: 'enable',
+        sub_field_answers: {
+          '.properties.tcp_routing.enable.reservable_ports' => {
+            attribute_value: '1024-1123',
+          },
+        },
+      )
+
+      networking_form.fill_in_selector_property(
+        selector_input_reference: '.properties.networking_point_of_entry',
+        selector_name: '',
+        selector_value: 'haproxy',
+        sub_field_answers: {}
+      )
+
+      networking_form.property('.ha_proxy.skip_cert_verify').set(elastic_runtime_settings['trust_self_signed_certificates'])
+      ssl_rsa_cert_property = '.properties.networking_point_of_entry][haproxy][.properties.networking_point_of_entry.haproxy.ssl_rsa_certificate'
+      if elastic_runtime_settings['ssl_certificate']
+        networking_form.nested_property(ssl_rsa_cert_property, 'cert_pem').set(elastic_runtime_settings['ssl_certificate'])
+        networking_form.nested_property(ssl_rsa_cert_property, 'private_key_pem').set(elastic_runtime_settings['ssl_private_key'])
+      else
+        domain = elastic_runtime_settings['system_domain']
+        networking_form.generate_self_signed_cert(
+          "*.#{domain},*.login.#{domain},*.uaa.#{domain}",
+          '.properties.networking_point_of_entry.haproxy.ssl_rsa_certificate',
+          '.properties.networking_point_of_entry',
+          'haproxy'
+        )
+      end
+
+      resource_config = current_ops_manager.product_resources_configuration(elastic_runtime_settings['name'])
+      resource_config.set_floating_ips_for_job('ha_proxy', elastic_runtime_settings['ha_proxy_floating_ips'])
+
+      resource_config = current_ops_manager.product_resources_configuration(elastic_runtime_settings['name'])
+      resource_config.set_floating_ips_for_job('tcp-router', elastic_runtime_settings['tcp_router_floating_ips'])
     end
+
+    networking_form.save_form
   end
 
   it 'configures smtp' do
@@ -75,102 +194,6 @@ RSpec.describe 'Configure Elastic Runtime 1.8.X', order: :defined do
   end
 
   private
-
-  def configure_vsphere_ha_proxy(elastic_runtime_settings)
-    networking_form =
-      current_ops_manager.product(elastic_runtime_settings['name']).product_form('networking')
-    networking_form.open_form
-    networking_form.property('.ha_proxy.static_ips').set(elastic_runtime_settings['ha_proxy_static_ips'])
-    networking_form.fill_in_selector_property(
-      selector_input_reference: '.properties.networking_point_of_entry',
-      selector_name: '',
-      selector_value: 'haproxy',
-      sub_field_answers: {}
-    )
-    configure_ssl_cert(networking_form, elastic_runtime_settings, 'haproxy')
-    networking_form.save_form
-  end
-
-  def configure_vsphere_tcp_routing(elastic_runtime_settings)
-    networking_form =
-      current_ops_manager.product(elastic_runtime_settings['name']).product_form('networking')
-    networking_form.open_form
-    networking_form.property('.tcp-router.static_ips').set(elastic_runtime_settings['tcp_router_static_ips'])
-    networking_form.save_form
-  end
-
-  def configure_aws_load_balancers(elastic_runtime_settings)
-    networking_form =
-      current_ops_manager.product(elastic_runtime_settings['name']).product_form('networking')
-    networking_form.open_form
-    networking_form.fill_in_selector_property(
-      selector_input_reference: '.properties.networking_point_of_entry',
-      selector_name: '',
-      selector_value: 'external_ssl',
-      sub_field_answers: {}
-    )
-    configure_ssl_cert(networking_form, elastic_runtime_settings, 'external_ssl')
-    networking_form.property('.properties.logger_endpoint_port').set('4443')
-    networking_form.save_form
-
-    resource_config = current_ops_manager.product_resources_configuration(elastic_runtime_settings['name'])
-    resource_config.set_instances_for_job('ha_proxy', 0)
-    resource_config.set_elb_names_for_job('router', elastic_runtime_settings['elb_name'])
-    resource_config.set_elb_names_for_job('diego_brain', elastic_runtime_settings['ssh_elb_name'])
-
-    # takes advantage of the pcf.<name>.cf-app.com format
-    env_name = env_settings['name'].split('.')[1]
-    tcp_elb_name = env_name + '-pcf-tcp-elb'
-    resource_config.set_elb_names_for_job('tcp-router', tcp_elb_name)
-
-    system_logging_form =
-      current_ops_manager.product(elastic_runtime_settings['name']).product_form('syslog_aggregator')
-    system_logging_form.open_form
-    system_logging_form.save_form
-  end
-
-  def configure_openstack_ha_proxy(elastic_runtime_settings)
-    networking_form =
-      current_ops_manager.product(elastic_runtime_settings['name']).product_form('networking')
-    networking_form.open_form
-    networking_form.fill_in_selector_property(
-      selector_input_reference: '.properties.networking_point_of_entry',
-      selector_name: '',
-      selector_value: 'haproxy',
-      sub_field_answers: {}
-    )
-    configure_ssl_cert(networking_form, elastic_runtime_settings, 'haproxy')
-    networking_form.save_form
-
-    resource_config = current_ops_manager.product_resources_configuration(elastic_runtime_settings['name'])
-    resource_config.set_floating_ips_for_job('ha_proxy', elastic_runtime_settings['ha_proxy_floating_ips'])
-  end
-
-  def configure_openstack_tcp_router(elastic_runtime_settings)
-    resource_config = current_ops_manager.product_resources_configuration(elastic_runtime_settings['name'])
-    resource_config.set_floating_ips_for_job('tcp-router', elastic_runtime_settings['tcp_router_floating_ips'])
-  end
-
-  def configure_ssl_cert(networking_form, elastic_runtime_settings, selector_option)
-    networking_form.property('.ha_proxy.skip_cert_verify').set(elastic_runtime_settings['trust_self_signed_certificates'])
-
-    ssl_rsa_cert_property = '.properties.networking_point_of_entry][' + \
-                            selector_option + '][.properties.networking_point_of_entry.' + \
-                            selector_option + '.ssl_rsa_certificate'
-
-    if elastic_runtime_settings['ssl_certificate']
-      networking_form.nested_property(ssl_rsa_cert_property, 'cert_pem').set(elastic_runtime_settings['ssl_certificate'])
-      networking_form.nested_property(ssl_rsa_cert_property, 'private_key_pem').set(elastic_runtime_settings['ssl_private_key'])
-    else
-      domain = elastic_runtime_settings['system_domain']
-      networking_form.generate_self_signed_cert(
-        "*.#{domain},*.login.#{domain},*.uaa.#{domain}",
-        '.properties.networking_point_of_entry.' + selector_option + '.ssl_rsa_certificate',
-        '.properties.networking_point_of_entry',
-        selector_option
-      )
-    end
-  end
 
   AVAILABILITY_ZONE_INPUT_SELECTOR = "input[name='product[availability_zone_references][]']"
 
