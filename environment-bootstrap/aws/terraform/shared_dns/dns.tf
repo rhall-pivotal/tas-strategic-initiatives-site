c9e81bce2e4052cf3c292bec45dfe7544f54a7cd
Aditya Anchuri <aanchuri@pivotal.io>
2016-08-22 17:52:49 -0700

    Remove duplicate AWS terraform templates

--- environment-bootstrap/aws/terraform/shared_dns/dns.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "pcf-ns" {
-  zone_id = "${var.shared_dns_hosted_zone_id}"
-  name = "${var.env_name}.cf-app.com"
-  type = "NS"
-  ttl = "300"
-  records = ["${var.releng_dns_hosted_zone_name_servers}"]
-}
--- environment-bootstrap/aws/terraform/shared_dns/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "env_name" {}
-variable "access_key" {}
-variable "secret_key" {}
-variable "region" {}
-variable "shared_dns_hosted_zone_id" {}
-variable "releng_dns_hosted_zone_name_servers" {}
