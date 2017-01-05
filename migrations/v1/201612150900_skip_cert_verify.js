bcacd77c60ae48c4204c19487b664a1cb7310e9a
bot <witchcraft@example.com>
2017-01-05 14:36:19 -0800

    Revert "Add a migration to move the skip_cert_verify value."
    This reverts commit 5b0ce8375db5178f367d2b1ec5f2f0bc8f0467c4.

--- migrations/v1/201612150900_skip_cert_verify.js
+++
@@ -1,7 +0,0 @@
-exports.migrate = function(input) {
-  var properties = input.properties;
-
-  properties['.properties.skip_cert_verify'] = properties['.ha_proxy.skip_cert_verify'];
-
-  return input;
-}
