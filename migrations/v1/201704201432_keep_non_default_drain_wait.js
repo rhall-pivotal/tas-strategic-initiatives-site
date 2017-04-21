63dfcb5663e8e6dfb761bd63b17148c85d9525d4
Kevin Kelani <kkelani@pivotal.io>
2017-04-21 12:30:16 -0700

    Revert "add migration to keep non default drain waits"
    This reverts commit 1e56f0661699ffdfc0b95e304b9a5b3d4feae5ad.

--- migrations/v1/201704201432_keep_non_default_drain_wait.js
+++
@@ -1,9 +0,0 @@
-exports.migrate = function(input) {
-  var properties = input.properties;
-
-  if (properties['.router.drain_wait'] != 0) {
-    properties['.router.drain_wait'] = properties['.router.drain_wait']
-  }
-
-  return input;
-};
