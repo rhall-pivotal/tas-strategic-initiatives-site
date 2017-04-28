ab7cf4ea91f11a5ca7ff619bee6d6e24cc45fd63
Zachary Gershman <zgershman@pivotal.io>
2017-04-27 17:01:01 -0700

    remove all the cruft from 1.11
    - we shouldn't need any of this stuff
    anymore

--- integration/integration_spec_helper.rb
+++
@@ -1,6 +0,0 @@
-require 'opsmgr/ui_helpers/config_helper'
-require 'capybara'
-
-SPEC_ROOT = File.expand_path(File.dirname(__FILE__))
-
-Capybara.save_and_open_page_path = File.expand_path(File.join(SPEC_ROOT, '..', 'tmp'))
