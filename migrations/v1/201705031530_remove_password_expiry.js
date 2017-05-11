77614249ccd860f4cdf5d80ba4972f80debfb666
Dave Walter <dwalter@pivotal.io>
2017-05-11 10:16:11 -0700

    Remove password expiry from UAA in 1.11
    - remove form field
    - add migration in auth/enterprise sso
    
    [#144041983]
    
    Signed-off-by: Zachary Gershman <zgershman@pivotal.io>

---
+++ migrations/v1/201705031530_remove_password_expiry.js
@@ -0,0 +1,6 @@
+exports.migrate = function(input) {
+    var properties = input.properties;
+    properties['.properties.uaa.internal.password_expires_after_months'] = {value: '0'};
+
+    return input;
+};
