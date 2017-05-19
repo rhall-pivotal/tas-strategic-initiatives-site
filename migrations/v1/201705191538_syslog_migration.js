885a44ef8122f1b270002e347932a94a1f60c236
Raina Masand <rmasand@pivotal.io>
2017-05-19 15:43:08 -0700

    set syslog config depending on selector
    [#144951383]
    
    Signed-off-by: Zachary Gershman <zgershman@pivotal.io>

---
+++ migrations/v1/201705191538_syslog_migration.js
@@ -0,0 +1,9 @@
+exports.migrate = function(input) {
+  var properties = input.properties;
+
+  properties['.properties.system_logging.enabled.host'] = properties['.properties.syslog_host'];
+  properties['.properties.system_logging.enabled.port'] = properties['.properties.syslog_port'];
+  properties['.properties.system_logging.enabled.protocol'] = properties['.properties.syslog_protocol'];
+
+  return input;
+};
