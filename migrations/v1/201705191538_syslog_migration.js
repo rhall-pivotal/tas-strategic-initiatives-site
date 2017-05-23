180c188c7e8473f03f901ac130064ae3e9a9cad6
Zachary Gershman <zgershman@pivotal.io>
2017-05-23 14:05:48 -0700

    conditionally enable syslog during migration
    [#143530161]
    
    Signed-off-by: Utako Ueda <uueda@pivotal.io>

--- migrations/v1/201705191538_syslog_migration.js
+++ migrations/v1/201705191538_syslog_migration.js
@@ -1,11 +1,14 @@
 exports.migrate = function(input) {
   var properties = input.properties;
 
-  properties['.properties.system_logging'] = {value: 'enabled'};
+  properties['.properties.system_logging'] = {value: 'disabled'};
 
-  properties['.properties.system_logging.enabled.host'] = properties['.properties.syslog_host'];
-  properties['.properties.system_logging.enabled.port'] = properties['.properties.syslog_port'];
-  properties['.properties.system_logging.enabled.protocol'] = properties['.properties.syslog_protocol'];
+  if(properties['.properties.syslog_host']['value']) {
+    properties['.properties.system_logging'] = {value: 'enabled'};
+    properties['.properties.system_logging.enabled.host'] = properties['.properties.syslog_host'];
+    properties['.properties.system_logging.enabled.port'] = properties['.properties.syslog_port'];
+    properties['.properties.system_logging.enabled.protocol'] = properties['.properties.syslog_protocol'];
+  }
 
   return input;
 };
