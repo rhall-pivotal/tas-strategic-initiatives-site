f9d466fc6e4aa574c111cabcabcc05cf1e025f94
Kevin Kelani <kkelani@pivotal.io>
2017-06-02 17:29:30 -0700

    Revert the syslog form changes
    - move host/port/protocol out of the selector as they are used by the
    p-mysql tile
    - turn the selector into an "enable_tls" selector
    - add a hidden selector to allow us to use named manifests for syslog
    configuration
    
    [#146551943]
    
    Signed-off-by: Dave Walter <dwalter@pivotal.io>

--- migrations/v1/201705191538_syslog_migration.js
+++ migrations/v1/201705191538_syslog_migration.js
@@ -1,14 +1,7 @@
 exports.migrate = function(input) {
   var properties = input.properties;
 
-  properties['.properties.system_logging'] = {value: 'disabled'};
-
-  if(properties['.properties.syslog_host']['value']) {
-    properties['.properties.system_logging'] = {value: 'enabled'};
-    properties['.properties.system_logging.enabled.host'] = properties['.properties.syslog_host'];
-    properties['.properties.system_logging.enabled.port'] = properties['.properties.syslog_port'];
-    properties['.properties.system_logging.enabled.protocol'] = properties['.properties.syslog_protocol'];
-  }
+  properties['.properties.syslog_tls'] = {value: 'disabled'};
 
   return input;
 };
