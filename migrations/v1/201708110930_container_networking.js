5cf6e7dc0404984c4d1e5207a29df9e389559a6b
Kalai Wei <kawei@pivotal.io>
2017-08-14 11:01:32 -0700

    Fix issues with migrations accessing migrations
    - that no longer exist
    
    [#150155322, #149539009]
    
    Signed-off-by: Dave Walter <dwalter@pivotal.io>

--- migrations/v1/201708110930_container_networking.js
+++ migrations/v1/201708110930_container_networking.js
@@ -1,9 +1,11 @@
 exports.migrate = function(input) {
   var properties = input.properties;
 
-  if( properties['.properties.container_networking.value'] == 'enable' ) {
-    properties['.properties.container_networking_network_cidr'] = properties['.properties.container_networking.enable.network_cidr'];
-    properties['.properties.container_networking_vtep_port'] = properties['.properties.container_networking.enable.vtep_port'];
+  if( properties['.properties.container_networking'] ) {
+    if( properties['.properties.container_networking']['value'] == 'enable' ) {
+      properties['.properties.container_networking_network_cidr'] = properties['.properties.container_networking.enable.network_cidr'];
+      properties['.properties.container_networking_vtep_port'] = properties['.properties.container_networking.enable.vtep_port'];
+    }
   }
 
   return input;
