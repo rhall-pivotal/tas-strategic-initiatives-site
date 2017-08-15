9503630aa43df13cec395ceebe5bf1887179405b
Kalai Wei <kawei@pivotal.io>
2017-08-15 10:58:55 -0700

    Set cipher suites outside of selector
    [#150181917]
    
    Signed-off-by: Rizwan Reza <rreza@pivotal.io>

--- migrations/v1/201708111927_move_vtep_port.js
+++ migrations/v1/201708111927_move_vtep_port.js
@@ -2,8 +2,8 @@ exports.migrate = function(input) {
   var properties = input.properties;
 
   if ( properties['.properties.container_networking']['value'] == 'enable' ) {
-    properties['.properties.container_networking_vtep_port'] = properties['.properties.container_networking.enable.vtep_port']
-    delete properties['.properties.container_networking.enable.vtep_port']
+    properties['.properties.container_networking_vtep_port'] = properties['.properties.container_networking.enable.vtep_port'];
+    delete properties['.properties.container_networking.enable.vtep_port'];
   }
 
   return input;
