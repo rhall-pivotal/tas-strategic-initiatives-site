b73c15cc64b9026105056a0a7182fdeb87448187
Dave Walter <dwalter@pivotal.io>
2017-08-15 08:19:57 -0700

    Fix issues with SSL cipher suite migration
    - the field is a colon-separated list of ciphers
    - the field could be null (empty)
    
    [#149538355]

--- migrations/v1/201708111140_ssl_cipher_for_haproxy_and_router.js
+++ migrations/v1/201708111140_ssl_cipher_for_haproxy_and_router.js
@@ -2,6 +2,7 @@ exports.migrate = function(input) {
   var properties = input.properties;
 
   var rfcToSSL = [
+    ["", ""],
     ["TLS_RSA_WITH_RC4_128_SHA", "RC4-SHA"],
     ["TLS_RSA_WITH_3DES_EDE_CBC_SHA", "DES-CBC3-SHA"],
     ["TLS_RSA_WITH_AES_128_CBC_SHA", "AES128-SHA"],
@@ -21,12 +22,19 @@ exports.migrate = function(input) {
     ["TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384", "ECDHE-ECDSA-AES256-GCM-SHA384"],
   ];
 
-  var cipherMap= new Map(rfcToSSL);
+  var cipherMap = new Map(rfcToSSL);
 
   if( properties['.properties.networking_point_of_entry'] ) {
     if( properties['.properties.networking_point_of_entry']['value'] == 'external_ssl' ) {
-      properties['.properties.gorouter_ssl_ciphers'] = {
-        value: cipherMap.get(properties['.properties.networking_point_of_entry.external_ssl.ssl_ciphers.value'])
+      if( properties['.properties.networking_point_of_entry.external_ssl.ssl_ciphers']['value'] != null ) {
+        var rfcCiphers = properties['.properties.networking_point_of_entry.external_ssl.ssl_ciphers']['value'].split(':')
+        var sslCiphers = rfcCiphers.map(function(c) {
+          return cipherMap.get(c)
+        })
+
+        properties['.properties.gorouter_ssl_ciphers'] = {
+          value: sslCiphers.join(':')
+        }
       }
     } else if ( properties['.properties.networking_point_of_entry']['value'] == 'haproxy' ) {
       properties['.properties.haproxy_ssl_ciphers'] = properties['.properties.networking_point_of_entry.haproxy.ssl_ciphers'];
