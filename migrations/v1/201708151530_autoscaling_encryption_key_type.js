e0768a30a9c128150262150cd6e47fa2e01aa9f0
Dave Walter <dwalter@pivotal.io>
2017-08-16 07:51:55 -0700

    Fix the autoscaling encryption_key migration.
    [#148116899]

--- migrations/v1/201708151530_autoscaling_encryption_key_type.js
+++ migrations/v1/201708151530_autoscaling_encryption_key_type.js
@@ -1,9 +1,7 @@
 exports.migrate = function(input) {
   var properties = input.properties;
 
-  properties['.autoscaling.encryption_key'] = {
-    value: properties['.autoscaling.encryption_key.haproxy.max_buffer_size']['password']
-  };
+  properties['.autoscaling.encryption_key']['value'] = properties['.autoscaling.encryption_key']['password'];
 
   return input;
 };
