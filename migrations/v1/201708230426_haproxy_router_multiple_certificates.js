5cacfa3aee7eff79bc99c6dd572ef612112567ca
Andrew Crump <acrump@pivotal.io>
2017-08-24 14:57:00 +1200

    Allow operator to configure multiple certificates
    [#149038943]

---
+++ migrations/v1/201708230426_haproxy_router_multiple_certificates.js
@@ -0,0 +1,14 @@
+exports.migrate = function(input) {
+  var properties = input.properties;
+
+  if (properties['.properties.networking_poe_ssl_cert']) {
+    properties['.properties.networking_poe_ssl_cert']['value'] = [
+      {
+        guid: { value: generateGuid() },
+        cert_chain: { value: properties['.properties.networking_poe_ssl_cert']['value']['cert_pem'] },
+        private_key: { value: properties['.properties.networking_poe_ssl_cert']['value']['private_key_pem'] }
+      }
+    ];
+  }
+  return input;
+};
