2ced703129b8414f9e2b94d48ffb1e2cba313dd6
Andrew Crump <acrump@pivotal.io>
2017-08-24 14:08:40 -0700

    Rename networking_poe_ssl_cert to networking_poe_ssl_certs
    [#149038943]
    
    Signed-off-by: Kalai Wei <kawei@pivotal.io>

--- migrations/v1/201708230426_haproxy_router_multiple_certificates.js
+++ migrations/v1/201708230426_haproxy_router_multiple_certificates.js
@@ -2,14 +2,17 @@ exports.migrate = function(input) {
   var properties = input.properties;
 
   if (properties['.properties.networking_poe_ssl_cert']) {
-    properties['.properties.networking_poe_ssl_cert']['value'] = [
-      {
-        guid: { value: generateGuid() },
-        name: { value: "Certficate" },
-        cert_chain: { value: properties['.properties.networking_poe_ssl_cert']['value']['cert_pem'] },
-        private_key: { value: properties['.properties.networking_poe_ssl_cert']['value']['private_key_pem'] }
-      }
-    ];
+    properties['.properties.networking_poe_ssl_certs'] = {
+      value: [
+        {
+          guid: { value: generateGuid() },
+          name: { value: "Certficate" },
+          cert_chain: { value: properties['.properties.networking_poe_ssl_cert']['value']['cert_pem'] },
+          private_key: { value: properties['.properties.networking_poe_ssl_cert']['value']['private_key_pem'] }
+        }
+      ]
+    };
+    delete properties['.properties.networking_poe_ssl_cert'];
   }
   return input;
 };
