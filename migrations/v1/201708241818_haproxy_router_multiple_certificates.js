a22c868e013984ca562da5a9fe7873f2ba8cae5d
Dave Walter <dwalter@pivotal.io>
2017-08-25 17:29:32 -0700

    Rename networking_poe_ssl_certs.
    - reflect the fact that it is now a collection of certs
    - consistent with the ERT
    
    [#150229967]

--- migrations/v1/201708241818_haproxy_router_multiple_certificates.js
+++ migrations/v1/201708241818_haproxy_router_multiple_certificates.js
@@ -2,14 +2,17 @@ exports.migrate = function(input) {
   var properties = input.properties;
 
   if (properties['.properties.networking_poe_ssl_cert']) {
-    properties['.properties.networking_poe_ssl_cert']['value'] = [
-      {
-        guid: { value: generateGuid() },
-        name: { value: "Cert" },
-        cert_chain: { value: properties['.properties.networking_poe_ssl_cert']['value']['cert_pem'] },
-        private_key: { value: properties['.properties.networking_poe_ssl_cert']['value']['private_key_pem'] }
-      }
-    ];
+    properties['.properties.networking_poe_ssl_certs'] = {
+      value: [
+        {
+          guid: { value: generateGuid() },
+          name: { value: "Certificate" },
+          cert_chain: { value: properties['.properties.networking_poe_ssl_cert']['value']['cert_pem'] },
+          private_key: { value: properties['.properties.networking_poe_ssl_cert']['value']['private_key_pem'] }
+        }
+      ]
+    };
   }
+
   return input;
 };
