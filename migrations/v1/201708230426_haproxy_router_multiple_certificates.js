1191893038f7d64a11071166f4b74b30f8c6f4e5
Dave Walter <dwalter@pivotal.io>
2017-09-01 10:46:38 -0700

    Revert "Rename networking_poe_ssl_cert to networking_poe_ssl_certs"
    This reverts commit 2ced703129b8414f9e2b94d48ffb1e2cba313dd6.
    
    [#150749594]

--- migrations/v1/201708230426_haproxy_router_multiple_certificates.js
+++ migrations/v1/201708230426_haproxy_router_multiple_certificates.js
@@ -2,17 +2,14 @@ exports.migrate = function(input) {
   var properties = input.properties;
 
   if (properties['.properties.networking_poe_ssl_cert']) {
-    properties['.properties.networking_poe_ssl_certs'] = {
-      value: [
-        {
-          guid: { value: generateGuid() },
-          name: { value: "Certficate" },
-          cert_chain: { value: properties['.properties.networking_poe_ssl_cert']['value']['cert_pem'] },
-          private_key: { value: properties['.properties.networking_poe_ssl_cert']['value']['private_key_pem'] }
-        }
-      ]
-    };
-    delete properties['.properties.networking_poe_ssl_cert'];
+    properties['.properties.networking_poe_ssl_cert']['value'] = [
+      {
+        guid: { value: generateGuid() },
+        name: { value: "Certficate" },
+        cert_chain: { value: properties['.properties.networking_poe_ssl_cert']['value']['cert_pem'] },
+        private_key: { value: properties['.properties.networking_poe_ssl_cert']['value']['private_key_pem'] }
+      }
+    ];
   }
   return input;
 };
