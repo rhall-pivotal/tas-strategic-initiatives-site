21758fde2846d1bcaf37f0c4563afb2240151be7
Michelle He <mhe@pivotal.io>
2017-11-07 16:01:37 -0800

    migrate credentials to credhub
    [#151144115]
    
    - deploy_autoscaling_broker_credentials
    - deploy_autoscaling_encryption_key
    
    Signed-off-by: Josh Zarrabi <jzarrabi@pivotal.io>

---
+++ migrations/common/201711071502_credhub_simple_credentials_2_0.js
@@ -0,0 +1,14 @@
+exports.migrate = function(input) {
+
+  input.variable_migrations.push({
+    from: input.properties['.properties.deploy_autoscaling_broker_credentials'],
+    to_variable: 'deploy-autoscaling-broker-credentials'
+  });
+
+  input.variable_migrations.push({
+    from: input.properties['.properties.deploy_autoscaling_encryption_key'],
+    to_variable: 'deploy-autoscaling-encryption-key'
+  });
+
+  return input;
+};
