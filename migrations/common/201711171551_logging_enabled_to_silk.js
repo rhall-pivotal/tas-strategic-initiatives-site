4fbc6e496f3cdd672cb04ea11548f9f83b284f85
Kevin Kelani <kkelani@pivotal.io>
2017-11-17 15:55:26 -0800

    moves options to enable logging under silk
    [#152938268]
    
    Signed-off-by: Kalai Wei <kawei@pivotal.io>

---
+++ migrations/common/201711171551_logging_enabled_to_silk.js
@@ -0,0 +1,10 @@
+exports.migrate = function(input) {
+  var properties = input.properties;
+
+  if (properties['.properties.container_networking_log_traffic']['value'] == 'enabled') {
+    properties['.properties.container_networking_interface_plugin.silk.enable_log_traffic'] = { value: true };
+    delete properties['.properties.container_networking_interface_plugin.silk.enable_log_traffic']
+  }
+
+  return input;
+};
