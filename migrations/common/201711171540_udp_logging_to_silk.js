dc34ee5fab82343756597f0c1a94fd47468212df
Kalai Wei <kawei@pivotal.io>
2017-11-17 20:28:23 -0800

    Fix migration typo in udp_logging
    [#152938103]

--- migrations/common/201711171540_udp_logging_to_silk.js
+++ migrations/common/201711171540_udp_logging_to_silk.js
@@ -1,7 +1,7 @@
 exports.migrate = function(input) {
   var properties = input.properties;
 
-  if (properties['.properties.container_networking_log_traffic']['value'] == 'enabled') {
+  if (properties['.properties.container_networking_log_traffic']['value'] == 'enable') {
     properties['.properties.container_networking_interface_plugin.silk.iptables_accepted_udp_logs_per_sec'] = properties['.properties.container_networking_log_traffic.enable.iptables_accepted_udp_logs_per_sec']
     delete properties['.properties.container_networking_log_traffic.enable.iptables_accepted_udp_logs_per_sec']
   }
