d2982ad49d752c95099374c734509cba4d058ed5
Kalai Wei <kawei@pivotal.io>
2017-11-17 20:28:24 -0800

    Fix migration to delete the correct property
    [#152938268]

--- migrations/common/201711171551_logging_enabled_to_silk.js
+++ migrations/common/201711171551_logging_enabled_to_silk.js
@@ -1,9 +1,9 @@
 exports.migrate = function(input) {
   var properties = input.properties;
 
-  if (properties['.properties.container_networking_log_traffic']['value'] == 'enabled') {
+  if (properties['.properties.container_networking_log_traffic']['value'] == 'enable') {
     properties['.properties.container_networking_interface_plugin.silk.enable_log_traffic'] = { value: true };
-    delete properties['.properties.container_networking_interface_plugin.silk.enable_log_traffic']
+    delete properties['.properties.container_networking_log_traffic']
   }
 
   return input;
