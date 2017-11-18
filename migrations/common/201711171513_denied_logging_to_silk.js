e42b90badafe90090441b064b03d3530bde4c993
Kalai Wei <kawei@pivotal.io>
2017-11-17 20:28:23 -0800

    Fix migration typo enable
    [#152938065]

--- migrations/common/201711171513_denied_logging_to_silk.js
+++ migrations/common/201711171513_denied_logging_to_silk.js
@@ -1,9 +1,9 @@
 exports.migrate = function(input) {
   var properties = input.properties;
 
-  if (properties['.properties.container_networking_log_traffic']['value'] == 'enabled') {
-    properties['.properties.container_networking_interface_plugin.silk.iptables_denied_logs_per_sec'] = properties['.properties.container_networking_log_traffic.enabled.iptables_denied_logs_per_sec']
-    delete properties['.properties.container_networking_log_traffic.enabled.iptables_denied_logs_per_sec']
+  if (properties['.properties.container_networking_log_traffic']['value'] == 'enable') {
+    properties['.properties.container_networking_interface_plugin.silk.iptables_denied_logs_per_sec'] = properties['.properties.container_networking_log_traffic.enable.iptables_denied_logs_per_sec']
+    delete properties['.properties.container_networking_log_traffic.enable.iptables_denied_logs_per_sec']
   }
 
   return input;
