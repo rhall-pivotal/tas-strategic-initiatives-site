5170038200497fb2acf1886b0d03a158273d7f8d
Andrew Crump <acrump@pivotal.io>
2018-02-26 17:08:41 -0800

    Backfill migration test for mysql_proxy shutdown_delay
    [#152705046]

---
+++ migrations/common/tests/201802211450_mysql_proxy_shutdown_delay_test.js
@@ -0,0 +1,27 @@
+require("tap").mochaGlobals()
+const should = require("should")
+const migration = require("../201802211450_mysql_proxy_shutdown_delay.js")
+
+describe("MySQL proxy shutdown delay", function() {
+
+  context("when the existing shutdown delay is zero", function() {
+    it("increases the shutdown delay to 30 seconds", function(){
+      migration.migrate(
+        { properties: { ".mysql_proxy.shutdown_delay": { "value": 0 } } }
+      ).should.deepEqual(
+        { properties: { ".mysql_proxy.shutdown_delay": { "value": 30 } } }
+      );
+    });
+  });
+
+  context("when the existing shutdown delay is non-zero", function() {
+    it("retains the existing value", function(){
+      migration.migrate(
+        { properties: { ".mysql_proxy.shutdown_delay": { "value": 60 } } }
+      ).should.deepEqual(
+        { properties: { ".mysql_proxy.shutdown_delay": { "value": 60 } } }
+      );
+    });
+  });
+
+});
