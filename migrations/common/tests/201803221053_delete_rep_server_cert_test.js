b4f1fce34ac1a0f59f8c0d70f99229643e71d1fd
Kalai Wei <kawei@pivotal.io>
2018-03-22 11:52:22 -0700

    Add alternative names to the rep agent certificate
    - Required for Diego v2
    - Renamed the existing certificate so that Ops Manager will re-generate
    the certificate
    - Added migration to remove old property
    
    [#155958511]
    
    Signed-off-by: Andrew Crump <acrump@pivotal.io>

---
+++ migrations/common/tests/201803221053_delete_rep_server_cert_test.js
@@ -0,0 +1,29 @@
+require("tap").mochaGlobals()
+const should = require("should")
+const migration = require("../201803221053_delete_rep_server_cert.js")
+
+describe("Remove pre-V2 rep agent certificate", function() {
+  original_hash = {
+    properties: {
+      ".properties.rep_server_cert": {
+        "type": "rsa_cert_credentials",
+        "configurable": false,
+        "credential": true,
+        "value": {
+          "private_key_pem": "some-private-key-pem",
+          "cert_pem": "some-cert-pem"
+        },
+        "optional": false
+      }
+    }
+  };
+
+  migrated_hash = {
+    properties: {},
+  };
+
+  it("removes rep_server_cert", function(){
+    migration.migrate(original_hash).should.deepEqual(migrated_hash);
+  });
+
+});
