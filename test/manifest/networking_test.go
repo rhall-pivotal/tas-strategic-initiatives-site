209825d83fee82adbfa9c1823fb5d7b2b615a08b
Dave Walter <dwalter@pivotal.io>
2018-05-23 15:23:51 -0700

    Respect configuration of silk/external config_{cni|plugin}_dir
    [#157799236] CNI plugin and config dir regression for 3rd party plugins
    
    Signed-off-by: Chris Hendrix <chendrix@pivotal.io>

---
+++ test/manifest/networking_test.go
@@ -0,0 +1,81 @@
+package manifest_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Networking", func() {
+	Describe("Container networking", func() {
+		var (
+			inputProperties map[string]interface{}
+		)
+
+		Context("when Silk is enabled", func() {
+			BeforeEach(func() {
+				inputProperties = map[string]interface{}{
+					".properties.container_networking_interface_plugin": "silk",
+				}
+			})
+
+			It("configures the cni_config_dir", func() {
+				manifest, err := product.RenderService.RenderManifest(inputProperties)
+				Expect(err).NotTo(HaveOccurred())
+
+				job, err := manifest.FindInstanceGroupJob("diego_cell", "garden-cni")
+				Expect(err).NotTo(HaveOccurred())
+
+				cniConfigDir, err := job.Property("cni_config_dir")
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(cniConfigDir).To(Equal("/var/vcap/jobs/silk-cni/config/cni"))
+			})
+
+			It("configures the cni_plugin_dir", func() {
+				manifest, err := product.RenderService.RenderManifest(inputProperties)
+				Expect(err).NotTo(HaveOccurred())
+
+				job, err := manifest.FindInstanceGroupJob("diego_cell", "garden-cni")
+				Expect(err).NotTo(HaveOccurred())
+
+				cniPluginDir, err := job.Property("cni_plugin_dir")
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(cniPluginDir).To(Equal("/var/vcap/packages/silk-cni/bin"))
+			})
+		})
+
+		Context("when External is enabled", func() {
+			BeforeEach(func() {
+				inputProperties = map[string]interface{}{
+					".properties.container_networking_interface_plugin": "external",
+				}
+			})
+
+			It("configures the cni_config_dir", func() {
+				manifest, err := product.RenderService.RenderManifest(inputProperties)
+				Expect(err).NotTo(HaveOccurred())
+
+				job, err := manifest.FindInstanceGroupJob("diego_cell", "garden-cni")
+				Expect(err).NotTo(HaveOccurred())
+
+				cniConfigDir, err := job.Property("cni_config_dir")
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(cniConfigDir).To(Equal("/var/vcap/jobs/cni/config/cni"))
+			})
+
+			It("configures the cni_plugin_dir", func() {
+				manifest, err := product.RenderService.RenderManifest(inputProperties)
+				Expect(err).NotTo(HaveOccurred())
+
+				job, err := manifest.FindInstanceGroupJob("diego_cell", "garden-cni")
+				Expect(err).NotTo(HaveOccurred())
+				cniPluginDir, err := job.Property("cni_plugin_dir")
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(cniPluginDir).To(Equal("/var/vcap/packages/cni/bin"))
+			})
+		})
+	})
+})
