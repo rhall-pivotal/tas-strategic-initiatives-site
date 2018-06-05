8c64b742be45d90cf64ba69d6d9f47c9f1a18747
Kalai Wei <kawei@pivotal.io>
2018-06-05 10:38:24 -0700

    Gorouter provides a client cert when talking with diego cell backends
    [#155046007] operator should be able to validate that backends that request a client cert from Gorouter will receive one
    
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- test/manifest/routing_test.go
+++ test/manifest/routing_test.go
@@ -90,4 +90,17 @@ var _ = Describe("Routing", func() {
 			})
 		})
 	})
+
+	// TODO: stop skipping once ops-manifest supports testing for credentials
+	XDescribe("Gorouter provides client certs in request to Diego cells", func() {
+		It("creates a backend cert_chain and private_key", func() {
+			manifest, err := product.RenderService.RenderManifest(map[string]interface{}{})
+			Expect(err).NotTo(HaveOccurred())
+
+			router, err := manifest.FindInstanceGroupJob("router", "gorouter")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(router.Property("router/backends/cert_chain")).NotTo(BeNil())
+			Expect(router.Property("router/backends/private_key")).NotTo(BeNil())
+		})
+	})
 })
