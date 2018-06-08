9bf5feb38da4b5e4bb7d3b602c126c2175c0e722
Andrew Crump <acrump@pivotal.io>
2018-06-07 19:04:11 -0700

    Add manifest test for notifications templating
    [#157092101] I can build p-runtime without access to pcf-releng-ci repo
    
    Signed-off-by: Kalai Wei <kawei@pivotal.io>

---
+++ test/manifest/notification_test.go
@@ -0,0 +1,34 @@
+package manifest_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Logging", func() {
+	var instanceGroup string
+
+	Describe("notifications", func() {
+
+		BeforeEach(func() {
+			if productName == "srt" {
+				instanceGroup = "control"
+			} else {
+				instanceGroup = "clock_global"
+			}
+		})
+
+		It("has a notifications job with default CF notification template", func() {
+			manifest, err := product.RenderService.RenderManifest(nil)
+			Expect(err).NotTo(HaveOccurred())
+
+			notifications, err := manifest.FindInstanceGroupJob(instanceGroup, "deploy-notifications")
+			Expect(err).NotTo(HaveOccurred())
+
+			template, err := notifications.Property("notifications/default_template")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(template).To(ContainSubstring("CF Notification: {{.Subject}}"))
+		})
+	})
+
+})
