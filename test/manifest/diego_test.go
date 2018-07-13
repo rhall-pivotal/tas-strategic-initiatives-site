2eaab2cb4f12c804a4d0e9f4d1ecfc0421893adf
Dave Walter <dwalter@pivotal.io>
2018-07-13 10:00:01 -0700

    Revert "Colocate mapfs job with nfsv3driver job in compute/diego_cell"
    - this is temporary while nfs-volume-release is pinned to a version that
    includes a mapfs package
    - it should be reverted when we unpin nfs-volume-release
    
    This reverts commit 16991673bd19fdfe6aec9f6c508cf3d12fbe9050.
    
    [#158989879] Unpin nfs-volume-release
    
    Signed-off-by: Caitlyn Yu <cyu@pivotal.io>

--- test/manifest/diego_test.go
+++ test/manifest/diego_test.go
@@ -58,25 +58,4 @@ var _ = Describe("Diego", func() {
 		})
 
 	})
-
-	Context("Persistence", func() {
-		BeforeEach(func() {
-			if productName == "srt" {
-				instanceGroup = "compute"
-			} else {
-				instanceGroup = "diego_cell"
-			}
-		})
-
-		It("colocates the nfsv3driver job with the mapfs job from the mapfs-release", func() {
-			manifest, err := product.RenderService.RenderManifest(nil)
-			Expect(err).NotTo(HaveOccurred())
-
-			_, err = manifest.FindInstanceGroupJob(instanceGroup, "nfsv3driver")
-			Expect(err).NotTo(HaveOccurred())
-
-			_, err = manifest.FindInstanceGroupJob(instanceGroup, "mapfs")
-			Expect(err).NotTo(HaveOccurred())
-		})
-	})
 })
