8641c7f34995b0205559a5850441868f9e55e5e7
Lyle Franklin <lfranklin@pivotal.io>
2018-07-24 18:01:16 +0100

    Upgrading to 2.3 fails if grootfs is not enabled
    Co-authored-by: Tom Godkin <tgodkin@pivotal.io>

---
+++ migrations/common/tests/201807240930_enforce_garden_shed_unselected_test.js
@@ -0,0 +1,27 @@
+require("tap").mochaGlobals()
+const should = require("should")
+
+abortMigration = function(msg) {
+  throw new Error(msg);
+};
+
+const migration = require("../201807240930_enforce_garden_shed_unselected.js")
+
+describe("Ensure user has deployed without Garden Shed enabled", function() {
+
+  it("raises an error if shed is enabled", function(){
+    (function () {
+      migration.migrate(
+        { properties: { ".properties.enable_grootfs": { "value": false } } }
+      )
+    }).should.throw('attempt to upgrade to PAS 2.3+ with GrootFS disabled, please enable GrootFS prior to upgrade by checking "Enable the GrootFS container image plugin for Garden RunC" in "Application Containers"');
+  });
+
+  it("no-ops if GrootFS is selected", function(){
+    (function () {
+      migration.migrate(
+        { properties: { ".properties.enable_grootfs": { "value": true } } }
+      )
+    }).should.not.throw();
+  });
+});
