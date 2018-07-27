81d259362d8f4c29aaf629190274a3706ce5342b
Dave Walter <dwalter@pivotal.io>
2018-07-27 10:34:29 -0700

    Add cf-cli release & cf-cli-6-linux job
    [#159212406] The CF CLI is installed as a BOSH release on VMs that run errands

---
+++ test/manifest/cf_cli_test.go
@@ -0,0 +1,26 @@
+package manifest_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("CF CLI", func() {
+	var instanceGroup string
+
+	BeforeEach(func() {
+		if productName == "srt" {
+			instanceGroup = "control"
+		} else {
+			instanceGroup = "clock_global"
+		}
+	})
+
+	It("colocates the cf-cli-6-linux job on the instance group used to run errands", func() {
+		manifest, err := product.RenderService.RenderManifest(nil)
+		Expect(err).NotTo(HaveOccurred())
+
+		_, err = manifest.FindInstanceGroupJob(instanceGroup, "cf-cli-6-linux")
+		Expect(err).NotTo(HaveOccurred())
+	})
+})
