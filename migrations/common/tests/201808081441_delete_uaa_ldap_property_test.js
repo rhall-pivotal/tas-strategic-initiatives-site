a521cf0f45bf2937dd997504a8b625316165d426
Genevieve Lesperance <glesperance@pivotal.io>
2018-08-08 14:58:13 -0700

    Add migration to remove property and add test.
    [#159412873]
    
    - Run npm install and npm test in the migrations/ dir.
    
    (cherry picked from commit 25ae776e5ff2c16fd9f5ddd810c701210d619a12)
    (cherry picked from commit 664c749c019dcd0d63b0f823d3eb86798de7a8f6)
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

---
+++ migrations/common/tests/201808081441_delete_uaa_ldap_property_test.js
@@ -0,0 +1,23 @@
+require("tap").mochaGlobals()
+const should = require("should")
+const migration = require("../201808081441_delete_uaa_ldap_property.js")
+
+describe("Remove UAA LDAP Server SSL Cert Alias", function() {
+  original_hash = {
+    properties: {
+      ".properties.uaa.ldap.server_ssl_cert_alias": {
+        "type": "string",
+        "configurable": true,
+        "optional": true
+      },
+    },
+  };
+
+  migrated_hash = {
+    properties: {},
+  };
+
+  it("removes uaa.ldap.server_ssl_cert_alias", function(){
+    migration.migrate(original_hash).should.deepEqual(migrated_hash);
+  });
+});
