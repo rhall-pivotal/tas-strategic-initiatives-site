07e590d18aeb554dd87c964e69bfec153b7d5bad
Andrew Crump <acrump@pivotal.io>
2018-08-16 20:40:17 -0700

    CredHub 2.0 requires key_properties
    - Previously we were relying on being able to specify the encryption key
      properties as siblings of provider
    - CredHub now requires them to be provided under a `key_properties` hash:
      https://www.pivotaltracker.com/story/show/158431599
    - Backfill tests for encryption keys
    
    [#159563422] pivotal-cf/lts-pas-issues #122: Bump to CredHub 2.0 in PAS 2.3
    
    Signed-off-by: Michelle He <mhe@pivotal.io>
    
    (cherry picked from commit 4f283a004723c29d638064d80f9931936a68945e)

---
+++ test/manifest/credhub_test.go
@@ -0,0 +1,82 @@
+package manifest_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("CredHub", func() {
+	var instanceGroup string
+
+	BeforeEach(func() {
+		if productName == "srt" {
+			instanceGroup = "control"
+		} else {
+			instanceGroup = "credhub"
+		}
+	})
+
+	Describe("encryption keys", func() {
+		Context("when there is a single internal key", func() {
+			It("configures credhub with the key", func() {
+				manifest, err := product.RenderService.RenderManifest(nil)
+				Expect(err).NotTo(HaveOccurred())
+
+				credhub, err := manifest.FindInstanceGroupJob(instanceGroup, "credhub")
+				Expect(err).NotTo(HaveOccurred())
+
+				keys, err := credhub.Property("credhub/encryption/keys")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(keys).To(HaveLen(1))
+
+				key := keys.([]interface{})[0].(map[interface{}]interface{})
+				Expect(key["provider_name"]).To(Equal("internal-provider"))
+				Expect(key["key_properties"]).To(HaveKeyWithValue("encryption_password", ContainSubstring("credhub_key_encryption_passwords/0/key.value")))
+				Expect(key["active"]).To(BeTrue())
+			})
+		})
+
+		Context("when there is an additional HSM key set as primary", func() {
+			It("configures credhub with the keys, with the HSM key marked as active", func() {
+				manifest, err := product.RenderService.RenderManifest(map[string]interface{}{
+					".properties.credhub_key_encryption_passwords": []map[string]interface{}{
+						{
+							"key": map[string]interface{}{
+								"secret": "some-credhub-password",
+							},
+							"name":     "internal key display name",
+							"primary":  false,
+							"provider": "internal",
+						},
+						{
+							"key": map[string]interface{}{
+								"secret": "hsm-provider-key-name",
+							},
+							"name":     "hsm key display name",
+							"primary":  true,
+							"provider": "hsm",
+						},
+					},
+				})
+				Expect(err).NotTo(HaveOccurred())
+
+				credhub, err := manifest.FindInstanceGroupJob(instanceGroup, "credhub")
+				Expect(err).NotTo(HaveOccurred())
+
+				keys, err := credhub.Property("credhub/encryption/keys")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(keys).To(HaveLen(2))
+
+				internalKey := keys.([]interface{})[0].(map[interface{}]interface{})
+				Expect(internalKey["provider_name"]).To(Equal("internal-provider"))
+				Expect(internalKey["key_properties"]).To(HaveKeyWithValue("encryption_password", ContainSubstring("credhub_key_encryption_passwords/0/key.value")))
+				Expect(internalKey["active"]).To(BeFalse())
+
+				hsmKey := keys.([]interface{})[1].(map[interface{}]interface{})
+				Expect(hsmKey["provider_name"]).To(Equal("hsm-provider"))
+				Expect(hsmKey["key_properties"]).To(HaveKeyWithValue("encryption_key_name", ContainSubstring("credhub_key_encryption_passwords/1/key.value")))
+				Expect(hsmKey["active"]).To(BeTrue())
+			})
+		})
+	})
+})
