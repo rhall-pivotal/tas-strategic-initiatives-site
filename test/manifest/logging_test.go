7fa4ef78daffd4a0e9bd4779dcd25b77845219fb
Genevieve Lesperance <glesperance@pivotal.io>
2018-09-19 14:54:25 -0700

    Add tags to emitted metrics by the metron agent.
    - Add variables file for wrt to make label a variable that we can grab
    and use for emitted metrics.
    
    [#159216525]
    
    Signed-off-by: Andrew Crump <acrump@pivotal.io>

--- test/manifest/logging_test.go
+++ test/manifest/logging_test.go
@@ -7,7 +7,6 @@ import (
 
 var _ = Describe("Logging", func() {
 	Describe("metron agent", func() {
-
 		It("sets defaults on the metron agent", func() {
 			manifest, err := product.RenderManifest(nil)
 			Expect(err).NotTo(HaveOccurred())
@@ -19,6 +18,29 @@ var _ = Describe("Logging", func() {
 			deploymentName, err := agent.Property("metron_agent/deployment")
 			Expect(err).NotTo(HaveOccurred())
 			Expect(deploymentName).To(Equal(""))
+
+			By("setting tags on the emitted metrics")
+			tags, err := agent.Property("metron_agent/tags")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(tags).To(HaveKeyWithValue("product", "Pivotal Application Service for Windows"))
+			Expect(tags).To(HaveKeyWithValue("product_version", MatchRegexp(`^\d+\.\d+\.\d+.*`)))
+			Expect(tags).To(HaveKeyWithValue("system_domain", Not(BeEmpty())))
+		})
+
+		Context("when placement tags are configured by the user", func() {
+			It("sets the placement tags on the emitted metrics", func() {
+				manifest, err := product.RenderManifest(map[string]interface{}{
+					".windows_diego_cell.placement_tags": "tag1,tag2",
+				})
+				Expect(err).NotTo(HaveOccurred())
+
+				agent, err := manifest.FindInstanceGroupJob("windows_diego_cell", "metron_agent_windows")
+				Expect(err).NotTo(HaveOccurred())
+
+				tags, err := agent.Property("metron_agent/tags")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(tags).To(HaveKeyWithValue("placement_tag", "tag1,tag2"))
+			})
 		})
 
 		Context("when the enable cf metric name is set to true (migration during upgrades)", func() {
@@ -37,5 +59,4 @@ var _ = Describe("Logging", func() {
 			})
 		})
 	})
-
 })
