71e53dfbed7b64af4e18abd8dba12798e027d549
Genevieve Lesperance <glesperance@pivotal.io>
2018-10-08 11:26:57 -0700

    Switch to chai for migration assertions.
    - Vulnerabilities in tap required us to update tap.
    - Breaking changes in recent versions of tap required us to switch from
    `should` to `chai` pkg.
    
    [#160527827]

--- migrations/tests/201809181044_metron_agent_deployment_name_test.js
+++ migrations/tests/201809181044_metron_agent_deployment_name_test.js
@@ -1,12 +1,12 @@
 require("tap").mochaGlobals()
-const should = require("should")
+const should = require("chai").should()
 const migration = require("../201809181044_metron_agent_deployment_name.js")
 
 describe("Metron Agent Deployment Name", function() {
   it("enables the cf metric name on upgrade", function(){
     migration.migrate(
       { properties: {} }
-    ).should.deepEqual(
+    ).should.deep.equal(
       { properties: { ".properties.enable_cf_metric_name": { "value": true } } }
     );
   });
