ba97eefbd45eb5290fc2142bace9eaa1f8f7e6ea
Dave Walter <dwalter@pivotal.io>
2018-10-09 18:34:12 -0700

    Convert the SMB volume services config
    - from a selector to a checkbox
    
    [#160431182](https://www.pivotaltracker.com/story/show/160431182)

--- test/manifest/smb_volume_test.go
+++ test/manifest/smb_volume_test.go
@@ -8,8 +8,8 @@ import (
 var _ = Describe("SMB volume service", func() {
 	var instanceGroup string
 
-	Context("when the SMB driver is enabled", func() {
-		It("enables the smbdriver job", func() {
+	Context("when SMB volume services are disabled", func() {
+		It("disables the smbdriver job", func() {
 			if productName == "srt" {
 				instanceGroup = "compute"
 			} else {
@@ -17,6 +17,29 @@ var _ = Describe("SMB volume service", func() {
 			}
 
 			manifest, err := product.RenderManifest(nil)
+			Expect(err).NotTo(HaveOccurred())
+
+			smbDriver, err := manifest.FindInstanceGroupJob(instanceGroup, "smbdriver")
+			Expect(err).NotTo(HaveOccurred())
+
+			smbDriverDisabled, err := smbDriver.Property("disable")
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(smbDriverDisabled).To(BeTrue())
+		})
+	})
+
+	Context("when SMB volume services are enabled", func() {
+		It("enables the smbdriver job", func() {
+			if productName == "srt" {
+				instanceGroup = "compute"
+			} else {
+				instanceGroup = "diego_cell"
+			}
+
+			manifest, err := product.RenderManifest(map[string]interface{}{
+				".properties.enable_smb_volume_driver": true,
+			})
 			Expect(err).NotTo(HaveOccurred())
 
 			smbDriver, err := manifest.FindInstanceGroupJob(instanceGroup, "smbdriver")
@@ -35,7 +58,9 @@ var _ = Describe("SMB volume service", func() {
 				instanceGroup = "uaa"
 			}
 
-			manifest, err := product.RenderManifest(nil)
+			manifest, err := product.RenderManifest(map[string]interface{}{
+				".properties.enable_smb_volume_driver": true,
+			})
 			Expect(err).NotTo(HaveOccurred())
 
 			uaa, err := manifest.FindInstanceGroupJob(instanceGroup, "uaa")
@@ -66,7 +91,9 @@ var _ = Describe("SMB volume service", func() {
 				instanceGroup = "credhub"
 			}
 
-			manifest, err := product.RenderManifest(nil)
+			manifest, err := product.RenderManifest(map[string]interface{}{
+				".properties.enable_smb_volume_driver": true,
+			})
 			Expect(err).NotTo(HaveOccurred())
 
 			credhub, err := manifest.FindInstanceGroupJob(instanceGroup, "credhub")
@@ -89,7 +116,9 @@ var _ = Describe("SMB volume service", func() {
 				instanceGroup = "clock_global"
 			}
 
-			manifest, err := product.RenderManifest(nil)
+			manifest, err := product.RenderManifest(map[string]interface{}{
+				".properties.enable_smb_volume_driver": true,
+			})
 			Expect(err).NotTo(HaveOccurred())
 
 			smbBrokerPush, err := manifest.FindInstanceGroupJob(instanceGroup, "smbbrokerpush")
@@ -116,29 +145,6 @@ var _ = Describe("SMB volume service", func() {
 			Expect(smbBrokerProperties).To(HaveKeyWithValue("password", "((smb-broker-credentials.password))"))
 			Expect(smbBrokerProperties).To(HaveKeyWithValue("skip_cert_verify", BeFalse()))
 			Expect(smbBrokerProperties).To(HaveKeyWithValue("syslog_url", ""))
-		})
-	})
-
-	Context("when the SMB driver is disabled", func() {
-		It("disables the smbdriver job", func() {
-			if productName == "srt" {
-				instanceGroup = "compute"
-			} else {
-				instanceGroup = "diego_cell"
-			}
-
-			manifest, err := product.RenderManifest(map[string]interface{}{
-				".properties.smb_volume_driver": "disable",
-			})
-			Expect(err).NotTo(HaveOccurred())
-
-			smbDriver, err := manifest.FindInstanceGroupJob(instanceGroup, "smbdriver")
-			Expect(err).NotTo(HaveOccurred())
-
-			smbDriverDisabled, err := smbDriver.Property("disable")
-			Expect(err).NotTo(HaveOccurred())
-
-			Expect(smbDriverDisabled).To(BeTrue())
 		})
 	})
 })
