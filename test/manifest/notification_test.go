fb3403becad50f373957217550191ca88b8e52e7
Gary Liu <galiu@pivotal.io>
2018-11-16 11:28:33 -0800

    Notifications component talks TLS to database
    [#162001860]
    
    (cherry picked from commit 62254b218abe9c13ac283d76079a757ad6b514c5)
    Signed-off-by: Lyle Franklin <lfranklin@pivotal.io>

--- test/manifest/notification_test.go
+++ test/manifest/notification_test.go
@@ -5,28 +5,54 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Logging", func() {
-	var instanceGroup string
+var _ = Describe("Notifications", func() {
+	var (
+		instanceGroup   string
+		inputProperties map[string]interface{}
+	)
 
-	Describe("notifications", func() {
+	BeforeEach(func() {
+		if productName == "srt" {
+			instanceGroup = "control"
+		} else {
+			instanceGroup = "clock_global"
+		}
+		inputProperties = map[string]interface{}{}
+	})
+
+	It("has a notifications job with default properties", func() {
+		manifest, err := product.RenderManifest(inputProperties)
+		Expect(err).NotTo(HaveOccurred())
+
+		notifications, err := manifest.FindInstanceGroupJob(instanceGroup, "deploy-notifications")
+		Expect(err).NotTo(HaveOccurred())
+
+		template, err := notifications.Property("notifications/default_template")
+		Expect(err).ToNot(HaveOccurred())
+		Expect(template).To(ContainSubstring("CF Notification: {{.Subject}}"))
+
+		caCert, err := notifications.Property("notifications/database/ca_cert")
+		Expect(err).ToNot(HaveOccurred())
+		Expect(caCert).To(BeNil())
+	})
+
+	Context("when the TLS checkbox is checked", func() {
 		BeforeEach(func() {
-			if productName == "srt" {
-				instanceGroup = "control"
-			} else {
-				instanceGroup = "clock_global"
+			inputProperties = map[string]interface{}{
+				".properties.enable_tls_to_internal_pxc": true,
 			}
 		})
 
-		It("has a notifications job with default CF notification template", func() {
-			manifest, err := product.RenderManifest(nil)
+		It("enables TLS", func() {
+			manifest, err := product.RenderManifest(inputProperties)
 			Expect(err).NotTo(HaveOccurred())
 
 			notifications, err := manifest.FindInstanceGroupJob(instanceGroup, "deploy-notifications")
 			Expect(err).NotTo(HaveOccurred())
 
-			template, err := notifications.Property("notifications/default_template")
+			caCert, err := notifications.Property("notifications/database/ca_cert")
 			Expect(err).ToNot(HaveOccurred())
-			Expect(template).To(ContainSubstring("CF Notification: {{.Subject}}"))
+			Expect(caCert).NotTo(BeEmpty())
 		})
 	})
 })
