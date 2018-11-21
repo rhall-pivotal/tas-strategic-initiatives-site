4f7715b639738a6d04651ac6e1e1ec3438608efc
Lyle Franklin <lfranklin@pivotal.io>
2018-11-21 10:09:00 -0800

    Decouple Diego intermediate CA from PAS tile
    - Avoids needing to upgrade IST and PAS at the same time
    
    [#161791558] **[Security Fix]** Rotate diego intermediate CA before current certificate expires

--- test/manifest/diego_test.go
+++ test/manifest/diego_test.go
@@ -176,11 +176,11 @@ var _ = Describe("Diego", func() {
 
 			caCert, err := rep.Property("diego/executor/instance_identity_ca_cert")
 			Expect(err).NotTo(HaveOccurred())
-			Expect(caCert).To(Equal("((/cf/diego-instance-identity-intermediate-ca-2018.certificate))"))
+			Expect(caCert).To(Equal("((diego-instance-identity-intermediate-ca-2018.certificate))"))
 
 			caKey, err := rep.Property("diego/executor/instance_identity_key")
 			Expect(err).NotTo(HaveOccurred())
-			Expect(caKey).To(Equal("((/cf/diego-instance-identity-intermediate-ca-2018.private_key))"))
+			Expect(caKey).To(Equal("((diego-instance-identity-intermediate-ca-2018.private_key))"))
 		})
 	})
 
