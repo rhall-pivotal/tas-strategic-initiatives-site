fe5e16a7d03d97b230b93d1a16c997a336a51615
Tom Godkin <tgodkin@pivotal.io>
2019-04-18 15:45:48 +0100

    Enable containerd_mode by default
    A way to opt-out of containerd mode is also provided in the app
    container config.

---
+++ test/manifest/garden_test.go
@@ -0,0 +1,79 @@
+package manifest_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/pivotal-cf/planitest"
+)
+
+var _ = Describe("Garden", func() {
+	var instanceGroup = "isolated_diego_cell"
+
+	It("enables containerd_mode by default", func() {
+		manifest := renderProductManifest(product, nil)
+		garden := findManifestInstanceGroupJob(manifest, instanceGroup, "garden")
+
+		containerdMode, err := garden.Property("garden/containerd_mode")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(containerdMode).To(BeTrue())
+	})
+
+	Context("opted out of containerd mode", func() {
+		It("disables containerd_mode", func() {
+			manifest := renderProductManifest(product, map[string]interface{}{
+				".properties.garden_containerd_opt_out": true,
+			})
+			garden := findManifestInstanceGroupJob(manifest, instanceGroup, "garden")
+
+			containerdMode, err := garden.Property("garden/containerd_mode")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(containerdMode).To(BeFalse())
+		})
+	})
+
+	Describe("grootfs garbage collection", func() {
+		It("sets the reserved disk space", func() {
+			manifest := renderProductManifest(product, nil)
+			garden := findManifestInstanceGroupJob(manifest, instanceGroup, "garden")
+
+			reservedInMB, err := garden.Property("grootfs/reserved_space_for_other_jobs_in_mb")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(reservedInMB).To(Equal(15360))
+		})
+
+		Context("reserved_space_for_other_jobs_in_mb is set", func() {
+			It("sets the reserved disk space", func() {
+				manifest := renderProductManifest(product, map[string]interface{}{
+					".properties.garden_disk_cleanup":                                              "reserved",
+					".properties.garden_disk_cleanup.reserved.reserved_space_for_other_jobs_in_mb": 15361,
+				})
+				garden := findManifestInstanceGroupJob(manifest, instanceGroup, "garden")
+
+				reservedInMB, err := garden.Property("grootfs/reserved_space_for_other_jobs_in_mb")
+				Expect(err).NotTo(HaveOccurred())
+				Expect(reservedInMB).To(Equal(15361))
+			})
+		})
+	})
+
+	It("ensures the standard root filesystems remain in the layer cache", func() {
+		manifest := renderProductManifest(product, nil)
+		garden := findManifestInstanceGroupJob(manifest, instanceGroup, "garden")
+
+		persistentImageList, err := garden.Property("garden/persistent_image_list")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(persistentImageList).To(ContainElement("/var/vcap/packages/cflinuxfs3/rootfs.tar"))
+	})
+})
+
+func renderProductManifest(p *planitest.ProductService, c map[string]interface{}) planitest.Manifest {
+	manifest, err := p.RenderManifest(c)
+	ExpectWithOffset(1, err).NotTo(HaveOccurred())
+	return manifest
+}
+
+func findManifestInstanceGroupJob(m planitest.Manifest, group, job string) planitest.Manifest {
+	manifest, err := m.FindInstanceGroupJob(group, job)
+	ExpectWithOffset(1, err).NotTo(HaveOccurred())
+	return manifest
+}
