7a018629d194bf2fa1cfd4acbf8c2c81fa1ed398
Paul Warren <paul.warren@emc.com>
2019-07-22 17:03:07 -0700

    Disable mapfs when nfs is disabled
    [#167299774] XTP with PAS to set the "disable" property on the mapfs job when NFS is disabled
    
    Signed-off-by: Carlos Iriarte <ciriarte@pivotal.io>

--- test/manifest/nfs_volume_test.go
+++ test/manifest/nfs_volume_test.go
@@ -244,6 +244,14 @@ var _ = Describe("NFS volume service", func() {
 			Expect(err).NotTo(HaveOccurred())
 
 			Expect(nfsV3DriverProperties).To(HaveKeyWithValue("disable", BeTrue()))
+
+			mapfsJob, err := manifest.FindInstanceGroupJob(instanceGroup, "mapfs")
+			Expect(err).NotTo(HaveOccurred())
+
+			mapfsDisableProperty, err := mapfsJob.Property("disable")
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(mapfsDisableProperty).To(BeTrue())
 		})
 	})
 
