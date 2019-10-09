c616a942a69ccdbb2fd511f27dd3641d4b251ee4
Caitlyn Yu <cyu@pivotal.io>
2019-10-09 09:33:47 -0600

    Remove system metrics agent and change scrape port
    [#168763960]
    
    Signed-off-by: Tom Chen <tochen@pivotal.io>

--- test/manifest/logging_test.go
+++ test/manifest/logging_test.go
@@ -1,8 +1,6 @@
 package manifest_test
 
 import (
-	"fmt"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/pivotal-cf/planitest"
@@ -10,31 +8,8 @@ import (
 
 var _ = Describe("Logging", func() {
 	var (
-		instanceGroups       []string = []string{"isolated_diego_cell", "isolated_ha_proxy", "isolated_router"}
-		getAllInstanceGroups func(planitest.Manifest) []string
+		instanceGroups []string = []string{"isolated_diego_cell", "isolated_ha_proxy", "isolated_router"}
 	)
-
-	getAllInstanceGroups = func(manifest planitest.Manifest) []string {
-		groups, err := manifest.Path("/instance_groups")
-		Expect(err).NotTo(HaveOccurred())
-
-		groupList, ok := groups.([]interface{})
-		Expect(ok).To(BeTrue())
-
-		names := []string{}
-		for _, group := range groupList {
-			groupName := group.(map[interface{}]interface{})["name"].(string)
-
-			// ignore VMs that only contain a single placeholder job, i.e. SF-PAS only VMs that are present but non-configurable in PAS build
-			jobs, err := manifest.Path(fmt.Sprintf("/instance_groups/name=%s/jobs", groupName))
-			Expect(err).NotTo(HaveOccurred())
-			if len(jobs.([]interface{})) > 1 {
-				names = append(names, groupName)
-			}
-		}
-		Expect(names).NotTo(BeEmpty())
-		return names
-	}
 
 	Describe("loggregator agent", func() {
 		It("sets tags on the loggregator agent", func() {
@@ -300,54 +275,6 @@ rule
 					Expect(err).NotTo(HaveOccurred())
 
 					Expect(syslogConfig).To(ContainSubstring(`if ($msg contains "DEBUG") then stop`))
-				}
-			})
-		})
-	})
-
-	Describe("system metrics agent", func() {
-		It("sets defaults on the system-metrics agent", func() {
-			manifest, err := product.RenderManifest(nil)
-			Expect(err).NotTo(HaveOccurred())
-
-			for _, ig := range instanceGroups {
-				agent, err := manifest.FindInstanceGroupJob(ig, "loggr-system-metrics-agent")
-				Expect(err).NotTo(HaveOccurred())
-
-				enabled, err := agent.Property("enabled")
-				Expect(err).ToNot(HaveOccurred())
-				Expect(enabled).To(BeTrue())
-
-				tlsProps, err := agent.Property("system_metrics/tls")
-				Expect(err).ToNot(HaveOccurred())
-				Expect(tlsProps).To(HaveKey("ca_cert"))
-				Expect(tlsProps).To(HaveKey("cert"))
-				Expect(tlsProps).To(HaveKey("key"))
-			}
-		})
-
-		Context("when the Operator disables the system-metrics agent", func() {
-			It("sets enabled to false", func() {
-				manifest, err := product.RenderManifest(map[string]interface{}{
-					".properties.system_metrics_enabled": false,
-				})
-				Expect(err).NotTo(HaveOccurred())
-
-				instanceGroups := getAllInstanceGroups(manifest)
-
-				for _, ig := range instanceGroups {
-					agent, err := manifest.FindInstanceGroupJob(ig, "loggr-system-metrics-agent")
-					Expect(err).NotTo(HaveOccurred())
-
-					enabled, err := agent.Property("enabled")
-					Expect(err).ToNot(HaveOccurred())
-					Expect(enabled).To(BeFalse())
-
-					tlsProps, err := agent.Property("system_metrics/tls")
-					Expect(err).ToNot(HaveOccurred())
-					Expect(tlsProps).To(HaveKey("ca_cert"))
-					Expect(tlsProps).To(HaveKey("cert"))
-					Expect(tlsProps).To(HaveKey("key"))
 				}
 			})
 		})
