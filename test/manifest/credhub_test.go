4de974d1c675e280b060ac75389ab1b7738b90f8
Carlos Iriarte <ciriarte@pivotal.io>
2019-12-04 11:57:59 -0800

    Update our tests to use 20 character credhub passwords
    Authored-by: Mark Stokan <mstokan@pivotal.io>
    
    [#169769001] **[Bug Fix]** Add length constraint to credhub internal encryption provider keys

--- test/manifest/credhub_test.go
+++ test/manifest/credhub_test.go
@@ -52,14 +52,14 @@ var _ = Describe("CredHub", func() {
 					".properties.credhub_internal_provider_keys": []map[string]interface{}{
 						{
 							"key": map[string]interface{}{
-								"secret": "passwordA",
+								"secret": "12345678901234567890",
 							},
 							"name":    "KeyA",
 							"primary": true,
 						},
 						{
 							"key": map[string]interface{}{
-								"secret": "passwordB",
+								"secret": "09876543210987654321",
 							},
 							"name":    "KeyB",
 							"primary": false,
@@ -111,7 +111,7 @@ var _ = Describe("CredHub", func() {
 				".properties.credhub_internal_provider_keys": []map[string]interface{}{
 					{
 						"key": map[string]interface{}{
-							"secret": "passwordA",
+							"secret": "12345678901234567890",
 						},
 						"name":    "KeyA",
 						"primary": false,
@@ -165,14 +165,14 @@ var _ = Describe("CredHub", func() {
 					".properties.credhub_internal_provider_keys": []map[string]interface{}{
 						{
 							"key": map[string]interface{}{
-								"secret": "passwordA",
+								"secret": "12345678901234567890",
 							},
 							"name":    "KeyA",
 							"primary": false,
 						},
 						{
 							"key": map[string]interface{}{
-								"secret": "passwordB",
+								"secret": "98765432109876543210",
 							},
 							"name":    "KeyB",
 							"primary": false,
