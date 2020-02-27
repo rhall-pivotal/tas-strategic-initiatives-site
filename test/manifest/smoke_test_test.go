14fad6e917755b1d75afb469b2719719cea9f7a9
Nancy Hsieh <nhsieh@pivotal.io>
2020-02-27 15:12:55 -0800

    Reverts 2 commits related to Pivotal smoke-tests
    This reverts commit 65bf9d926af071dfeec92167be7498b4b82797ca.
    This reverts commit a864eb818334d8cf1d9ad88907710180bfde7dda.

--- test/manifest/smoke_test_test.go
+++ test/manifest/smoke_test_test.go
@@ -14,9 +14,22 @@ var _ = Describe("SmokeTest", func() {
 		Expect(err).NotTo(HaveOccurred())
 
 		By("reusing an existing org")
-		org, err := smokeTest.Property("smoke_tests/organization")
+		useExistingOrgProp, err := smokeTest.Property("smoke_tests/use_existing_org")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(useExistingOrgProp).To(BeTrue())
+		org, err := smokeTest.Property("smoke_tests/org")
 		Expect(err).NotTo(HaveOccurred())
 		Expect(org).To(Equal("system"))
+
+		By("reusing an existing space")
+		useExistingSpaceProp, err := smokeTest.Property("smoke_tests/use_existing_space")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(useExistingSpaceProp).To(BeFalse())
+
+		By("enabling windows tests")
+		enableWindowsTestProp, err := smokeTest.Property("smoke_tests/enable_windows_tests")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(enableWindowsTestProp).To(BeTrue())
 
 		By("setting the windows stack")
 		windowsStack, err := smokeTest.Property("smoke_tests/windows_stack")
@@ -27,9 +40,10 @@ var _ = Describe("SmokeTest", func() {
 	Context("when the operator specifies properties", func() {
 		It("configures the smoke-test erand", func() {
 			manifest, err := product.RenderManifest(map[string]interface{}{
-				".properties.smoke_tests_windows":                      "specified",
-				".properties.smoke_tests_windows.specified.org_name":   "banana",
-				".properties.smoke_tests_windows.specified.space_name": "banana",
+				".properties.smoke_tests_windows":                       "specified",
+				".properties.smoke_tests_windows.specified.org_name":    "banana",
+				".properties.smoke_tests_windows.specified.space_name":  "banana",
+				".properties.smoke_tests_windows.specified.apps_domain": "banana",
 			})
 			Expect(err).NotTo(HaveOccurred())
 
@@ -37,14 +51,25 @@ var _ = Describe("SmokeTest", func() {
 			Expect(err).NotTo(HaveOccurred())
 
 			By("reusing an existing org")
-			org, err := smokeTest.Property("smoke_tests/organization")
+			useExistingOrgProp, err := smokeTest.Property("smoke_tests/use_existing_org")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(useExistingOrgProp).To(BeTrue())
+			org, err := smokeTest.Property("smoke_tests/org")
 			Expect(err).NotTo(HaveOccurred())
 			Expect(org).To(Equal("banana"))
 
 			By("reusing an existing space")
+			useExistingSpaceProp, err := smokeTest.Property("smoke_tests/use_existing_space")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(useExistingSpaceProp).To(BeTrue())
 			space, err := smokeTest.Property("smoke_tests/space")
 			Expect(err).NotTo(HaveOccurred())
 			Expect(space).To(Equal("banana"))
+
+			By("enabling windows tests")
+			enableWindowsTestProp, err := smokeTest.Property("smoke_tests/enable_windows_tests")
+			Expect(err).NotTo(HaveOccurred())
+			Expect(enableWindowsTestProp).To(BeTrue())
 
 			By("setting the windows stack")
 			windowsStack, err := smokeTest.Property("smoke_tests/windows_stack")
