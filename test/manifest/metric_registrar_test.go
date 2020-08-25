2453c325ce7d9c8b661bf3fdd42e16ebd954ab3a
Louie Brann <lbrann@vmware.com>
2020-08-25 17:00:53 -0600

    update metric-registrar to 1.2.1
    [#174384868]

--- test/manifest/metric_registrar_test.go
+++ test/manifest/metric_registrar_test.go
@@ -41,6 +41,12 @@ var _ = Describe("Metric Registrar", func() {
 			return endpointWorker
 		}
 
+		var getSecureEndpointWorker = func(manifest planitest.Manifest) planitest.Manifest {
+			endpointWorker, err := manifest.FindInstanceGroupJob(workerInstanceGroup, "metric_registrar_secure_endpoint_worker")
+			Expect(err).ToNot(HaveOccurred())
+			return endpointWorker
+		}
+
 		Context("defaults", func() {
 			It("sets orchestrator defaults", func() {
 				manifest, err := product.RenderManifest(nil)
@@ -83,6 +89,69 @@ var _ = Describe("Metric Registrar", func() {
 				Expect(err).ToNot(HaveOccurred())
 				Expect(scrapeInterval).To(Equal("35s"))
 			})
+
+			It("sets secure endpoint worker defaults", func() {
+				manifest, err := product.RenderManifest(nil)
+				Expect(err).ToNot(HaveOccurred())
+
+				secureEndpointWorker := getSecureEndpointWorker(manifest)
+
+				disabled, err := secureEndpointWorker.Property("disabled")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(disabled).To(Equal(false))
+
+				blacklistedTags, err := secureEndpointWorker.Property("blacklisted_tags")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(blacklistedTags).To(ConsistOf("deployment", "job", "index", "id"))
+
+				scrapeInterval, err := secureEndpointWorker.Property("scrape_interval")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(scrapeInterval).To(Equal("35s"))
+
+				bbsTlsProps, err := secureEndpointWorker.Property("bbs/tls")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(bbsTlsProps).To(HaveKey("ca"))
+				Expect(bbsTlsProps).To(HaveKey("cert"))
+				Expect(bbsTlsProps).To(HaveKey("key"))
+
+				scrape, err := secureEndpointWorker.Property("scrape")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(scrape).To(HaveKey("diego_identity_ca"))
+				Expect(scrape).To(HaveKey("tls"))
+
+				scrapeTls, err := secureEndpointWorker.Property("scrape/tls")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(scrapeTls).To(HaveKey("ca"))
+				Expect(scrapeTls).To(HaveKey("cert"))
+				Expect(scrapeTls).To(HaveKey("key"))
+			})
+
+			It("sets up all jobs on different ports", func() {
+				manifest, err := product.RenderManifest(nil)
+				Expect(err).ToNot(HaveOccurred())
+
+				allJobs := []planitest.Manifest{
+					getEndpointWorker(manifest),
+					getSecureEndpointWorker(manifest),
+					getOrchestrator(manifest),
+					getLogWorker(manifest),
+				}
+
+				allPorts := make(map[interface{}]bool)
+				for _, job := range allJobs {
+					healthCheckPort, err := job.Property("health_check_port")
+					Expect(err).ToNot(HaveOccurred())
+					_, exists := allPorts[healthCheckPort]
+					Expect(exists).To(BeFalse())
+					allPorts[healthCheckPort] = true
+
+					orchestrationPort, err := job.Property("orchestration_port")
+					Expect(err).ToNot(HaveOccurred())
+					_, exists = allPorts[orchestrationPort]
+					Expect(exists).To(BeFalse())
+					allPorts[orchestrationPort] = true
+				}
+			})
 		})
 
 		When("operator disables metric registrar", func() {
@@ -103,6 +172,10 @@ var _ = Describe("Metric Registrar", func() {
 				disabled, err = getEndpointWorker(manifest).Property("disabled")
 				Expect(err).ToNot(HaveOccurred())
 				Expect(disabled).To(Equal(true))
+
+				disabled, err = getSecureEndpointWorker(manifest).Property("disabled")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(disabled).To(Equal(true))
 			})
 		})
 
@@ -114,6 +187,17 @@ var _ = Describe("Metric Registrar", func() {
 				Expect(err).ToNot(HaveOccurred())
 
 				scrapeInterval, err := getEndpointWorker(manifest).Property("scrape_interval")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(scrapeInterval).To(Equal("200s"))
+			})
+
+			It("propagates to the secure endpoint worker", func() {
+				manifest, err := product.RenderManifest(map[string]interface{}{
+					".properties.metric_registrar_scrape_interval_in_seconds": 200,
+				})
+				Expect(err).ToNot(HaveOccurred())
+
+				scrapeInterval, err := getSecureEndpointWorker(manifest).Property("scrape_interval")
 				Expect(err).ToNot(HaveOccurred())
 				Expect(scrapeInterval).To(Equal("200s"))
 			})
@@ -145,6 +229,10 @@ var _ = Describe("Metric Registrar", func() {
 				Expect(blacklistedTags).To(ConsistOf("tag1", "tag2"))
 
 				blacklistedTags, err = getEndpointWorker(manifest).Property("blacklisted_tags")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(blacklistedTags).To(ConsistOf("tag1", "tag2"))
+
+				blacklistedTags, err = getSecureEndpointWorker(manifest).Property("blacklisted_tags")
 				Expect(err).ToNot(HaveOccurred())
 				Expect(blacklistedTags).To(ConsistOf("tag1", "tag2"))
 			})
